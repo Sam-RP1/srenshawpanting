049e8c03242be90758a67a1a6bbbc05512f0b37a
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2020-08-11 15:05:21 +0100

    Major Changes
    Multi line ellipsis achieved and mixin created. gallery.scss include many more tailored break points for scaling the multi line ellipsis to display cleanly.

--- src/portfolio.js
+++ src/portfolio.js
@@ -13,8 +13,8 @@ import logoLight from './assets/logo/logo.png';
 import dssFile_1 from './assets/portfolio/distributed-systems/files/dss-report.pdf';
 const dssData = {
   id: "dss-report",
-  title: "distributed systems & security",
-  purpose: "university research paper",
+  title: "distributed systems",
+  purpose: "university research",
   description: "A paper in which methods to detect and mitigate DDOS attacks are researched, compared and evaluated for their suitability to be used by IoT temperature sensing devices as a DDOS security mechanism. The paper presents a basic lightweight algorithm that was tested and can be used by IoT temperature sensing devices to detect high volume DDOS attacks and mitigate their effects.",
   url: dssFile_1,
   button: "View Paper",
@@ -44,7 +44,7 @@ const fredData = {
 const prpData = {
   id: "peer-review-portal",
   title: "peer review portal",
-  purpose: "university individual project",
+  purpose: "university project",
   description: "A web application created to allow both peer review and meta-review to be conducted comprehensively in an online environment. The web application has two types of users, students and teachers, both of which are provided with features that enable them to assume their respective role and carry out their respective processes. The web application is also equipped with with a number of functions that operate autonomously.",
   url: "/portfolio-view.html?projectid=peerreviewportal",
   button: "View Project",
@@ -55,7 +55,7 @@ import roboticsFile_1 from './assets/portfolio/robotics/files/robotics-report.pd
 const roboticsData = {
   id: "robotics",
   title: "robotics",
-  purpose: "university individual project",
+  purpose: "university project",
   description: "A individual project with the aim to improve the performance of a mobile robots object manipulation and sorting, based on colour, in a simulated scenario of an industrial environment. The mobile robots performance improvements were achieved through designing various procedures and algorithms, along with implementing specific hardwares to facilitate and enhance functionalities.",
   url: roboticsFile_1,
   button: "View Paper",
@@ -76,7 +76,7 @@ import pudFile_1 from './assets/portfolio/portsmouth-unattended-displays/files/R
 const pudData = {
   id: "pudhub",
   title: "pud hub",
-  purpose: "university individual project",
+  purpose: "university project",
   description: "The Portsmouth Unattended Displays Hub, a web application built to allow for the creation, customisation and management of unattended displays from a single place. In order for a display to be connected as an unattended display it must navigate to the web applications root address where it will be setup as. The web application has a number of features and functionalities like being able to create, edit and save HTML, CSS and JS files using a basic in browser text editor.",
   url: pudFile_1,
   button: "View Project",
@@ -86,8 +86,8 @@ const pudData = {
 import reviewFile_1 from './assets/portfolio/literature-review-2017/files/litrev.pdf';
 const reviewData = {
   id: "litreview",
-  title: "mitigating information exposure",
-  purpose: "university literature review",
+  title: "mitigating data exposure",
+  purpose: "university literature",
   description: "A literature review exploring the topic of Mitigating Corporate Information Exposure on the Web with a specific focus on Bring Your Own Device. The literature review evaluates some of the risks posed to corporations and the mitigation techniques used or put in place by corporations in order to prevent the exposure of information on the web.",
   url: reviewFile_1,
   button: "View Literature Review",
