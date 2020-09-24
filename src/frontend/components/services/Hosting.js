a90e706ed24be55c83df079ce425cfa5e8337d07
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2020-09-24 17:30:07 +0100

    Minor Text Changes

--- src/frontend/components/services/Hosting.js
+++ src/frontend/components/services/Hosting.js
@@ -23,17 +23,16 @@ const Hosting = () => {
     <ul>
     <li>Includes -</li>
     <ul>
-    <li>Deployment in one region</li>
+    <li>Deployment in one region of your choice</li>
     </ul>
     <li>Price -</li>
     <ul>
     <li>Setup Fee: £10.00</li>
-    <li>Yearly Fee: £30.00</li>
+    <li>Yearly Fee: £40.00</li>
     </ul>
-    <li>Best for static websites that...</li>
+    <li>Best for serverless websites that...</li>
     <ul>
     <li>Experience low to moderate traffic in a specific region</li>
-    <li>Have no server code</li>
     </ul>
     </ul>
     </div>
@@ -44,42 +43,47 @@ const Hosting = () => {
     <ul>
     <li>Includes -</li>
     <ul>
-    <li>Deployment in two regions (more regions can be added)</li>
+    <li>Deployment in two regions of your choice</li>
     </ul>
     <li>Price -</li>
     <ul>
     <li>Setup Fee: £15.00</li>
-    <li>Yearly Fee: £50.00</li>
-    <li>Extra Region Fee Per Year: £20.00</li>
+    <li>Yearly Fee: £60.00</li>
     </ul>
-    <li>Best for static websites that...</li>
+    <li>Best for serverless websites that...</li>
     <ul>
     <li>Experience moderate traffic across different regions</li>
-    <li>Have no server code</li>
+    <li>Need to display specific content to different regions</li>
     </ul>
     </ul>
     </div>
 
     <div>
     <h2>Custom Hosting</h2>
-    <p>The complete specialist hosting package that can be tailored to meet your unique requirements!</p>
-    <p>Need enterprise level hosting? Want a solution that scales resources dynamically to meet fluctuating traffic? Want a specific backend or server configuration?</p>
+    <p>The complete hosting package that can be tailored to meet your requirements.</p>
+    <p>Need enterprise level hosting? Need a specific backend or server configuration? Want a solution that scales resources dynamically to meet fluctuating traffic? Want hosting in multiple regions?</p>
     <p>Then this is the hosting package for you.</p>
     <ul>
-    <li>Price: Variable</li>
-    <li>Best for solutions that require...</li>
+    <li>Price - </li>
+    <ul>
+    <li>Setup Fee: £10.00 per hour</li>
+    <li>Yearly Fee: Variable</li>
+    </ul>
+    <li>For solutions requiring one or more of the following...</li>
     <ul>
     <li>Scalability</li>
-    <li>High Capacity</li>
-    <li>Enterprise Performance</li>
-    <li>Custom Infrastructure</li>
-    <li>Custom Server Configuration</li>
+    <li>Hosting in multiple regions</li>
+    <li>Enterprise performance</li>
+    <li>Custom infrastructure</li>
+    <li>Custom server configuration</li>
     </ul>
     </ul>
     </div>
     </section>
 
-    <p style={{marginBottom: "10px"}}>In order to host your website you will require a domain name. Domain names can be purchased from a number of retailers and cost a specific fee per year to keep. When hosting your website with SRP Designs we require your domain name to be handled by us so that we can configure it as necessary. If you already own your desired domain name we will require it to be transfered to us and if you do not own it then we can purchase it on your behalf. While a domain name is being managed by us we will invoice you yearly for the cost to keep it.</p>
+    <p className="small-print">***A domain name is required to host your website. Domain names can be purchased online for a specific fee per year.</p>
+
+    <p>When hosting your website with SRP Designs we will require your domain name to be handled by us so that it can be configured as necessary. If you already own a domain name then we will require it to be transfered to us. However, if you do not already own your desired domain name don't panic! We can purchase it on your behalf, making the process much easier. While a domain name is in our possession we will invoice you yearly for the cost to keep it.</p>
     <p>For all questions, queries or enquiries related to the hosting packages offered here please use the <a href="/contact.html"><u><i>contact form</i></u></a> and select the "Website Hosting" option from the "Select a Service" drop-down menu.</p>
     </article>
     </div>
