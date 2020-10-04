f497ec1903a7317bdf8fabf814a9c4369e1a76f1
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2020-10-05 00:19:03 +0100

    Minor changes
    Needs cleaning up and optimising. Certain aspects can be extracted into their own methods.

--- src/frontend/scripts/handlePages.js
+++ src/frontend/scripts/handlePages.js
@@ -5,19 +5,24 @@
 * @param {int} action Increment or decrement the page (+1 or -1)
 * @param {string} animation The name of the transition animation to perform
 * @param {string} id The current id of the open page, follows the format NAME-page-X
-* @param {callback} callback A passed function that allows the callers state to be updated with the newly opened pages id
+* @param {callback} callback A passed function that allows the callers states to be updated
 */
 const handlePages = (numPages, action, animation, id, callback) => {
   const splitName = id.split("-");
   const currentPageNum = parseInt(splitName[2]);
-  let nextPageNum;
+  const nextPageNum = currentPageNum + action; // Increment or decrement one page
+  let leftBtn;
+  let rightBtn;
 
-  if (action === -1 && currentPageNum === 0) {
-    nextPageNum = numPages - 1; // Skip to last page
-  } else if (action === 1 && (currentPageNum + 1) === numPages) {
-    nextPageNum = 0; // Skip to first page
+  if (nextPageNum === 0) {
+    leftBtn = false;
+    rightBtn = true;
+  } else if ((nextPageNum + 1) === numPages) {
+    leftBtn = true;
+    rightBtn = false;
   } else {
-    nextPageNum = currentPageNum + action; // Increment or decrement one page
+    leftBtn = true;
+    rightBtn = true;
   }
 
   const nextPageId = splitName[0] + "-" + splitName[1] + "-" + nextPageNum.toString();
@@ -26,18 +31,30 @@ const handlePages = (numPages, action, animation, id, callback) => {
   const nextPage = document.getElementById(nextPageId);
 
   // Transition
-  nextPage.classList.toggle("open");
-  currentPage.classList.toggle("close");
-
-  setTimeout(() => {
-    nextPage.classList.toggle("start");
+  if ((' ' + nextPage.className + ' ').indexOf(' ' + 'end' + ' ') > -1 === true) {
+    currentPage.classList.toggle("reverse");
     nextPage.classList.toggle("open");
-    currentPage.classList.toggle("start");
+
+    setTimeout(() => {
+      currentPage.classList.toggle("start");
+      currentPage.classList.toggle("reverse");
+      nextPage.classList.toggle("end");
+      nextPage.classList.toggle("open");
+    }, 1310)
+  } else {
+    nextPage.classList.toggle("open");
     currentPage.classList.toggle("close");
-  }, 1310)
+
+    setTimeout(() => {
+      nextPage.classList.toggle("start");
+      nextPage.classList.toggle("open");
+      currentPage.classList.toggle("end");
+      currentPage.classList.toggle("close");
+    }, 1310)
+  }
 
   // Update callers state
-  callback(nextPageId);
+  callback(nextPageId, leftBtn, rightBtn);
 }
 
 export default handlePages;
