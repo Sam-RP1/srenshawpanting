98727a20548579c6efb0f448e3879ae4816d5fd7
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2020-10-04 18:47:06 +0100

    Added animation parameter
    Not currently used, but will allow the caller to choose its transition animation.

--- src/frontend/scripts/handlePages.js
+++ src/frontend/scripts/handlePages.js
@@ -3,10 +3,11 @@
 * Handles the changing of pages and their transitions.
 * @param {int} numPages The total number of pages
 * @param {int} action Increment or decrement the page (+1 or -1)
+* @param {string} animation The name of the transition animation to perform
 * @param {string} id The current id of the open page, follows the format NAME-page-X
 * @param {callback} callback A passed function that allows the callers state to be updated with the newly opened pages id
 */
-const handlePages = (numPages, action, id, callback) => {
+const handlePages = (numPages, action, animation, id, callback) => {
   const splitName = id.split("-");
   const currentPageNum = parseInt(splitName[2]);
   let nextPageNum;
@@ -25,10 +26,15 @@ const handlePages = (numPages, action, id, callback) => {
   const nextPage = document.getElementById(nextPageId);
 
   // Transition
-  currentPage.classList.toggle("open");
+  nextPage.classList.toggle("open");
+  currentPage.classList.toggle("close");
+
   setTimeout(() => {
+    nextPage.classList.toggle("start");
     nextPage.classList.toggle("open");
-  }, 350)
+    currentPage.classList.toggle("start");
+    currentPage.classList.toggle("close");
+  }, 1310)
 
   // Update callers state
   callback(nextPageId);
