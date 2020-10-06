a942abe97ad9c17735d5da750d59218d43a15141
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2020-10-06 17:54:28 +0100

    Added function documentation

--- src/frontend/scripts/debounce.js
+++ src/frontend/scripts/debounce.js
@@ -1,3 +1,11 @@
+/**
+* debounce(lastEventTime, delay, callback) -
+* Decides if an event has been called too recently and should therefore not be called again.
+* @param {int} lastEventTime The time the last event took place
+* @param {int} delay The delay that needs to be exceeded for the event to take place
+* @param {callback} callback An optional parameter containing a passed function that allows the callers state to be updated if necessary
+* @return {bool} True or false depending on logic outcome
+*/
 const debounce = (lastEventTime, delay, callback) => {
   const currentTime = Date.now();
   if (currentTime - lastEventTime > delay) {
