cbe0c0e66ff46a1fb459a4bfc25172e6e210f08a
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2021-03-21 15:25:23 +0000

    update: srenshawpanting website will now use React + Typescript + Jest + eslint + Babel + webpack
    Changes made to these files are to accommodate for the project now using typescript with react.

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,17 +1,11 @@
 const path = require('path');
 const webpack = require('webpack');
+const ESLintPlugin = require('eslint-webpack-plugin');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: {
-        index: ['webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000', './src/frontend/index.js'],
-        portfolio: ['webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000', './src/frontend/portfolio.js'],
-        portfolio_view: [
-            'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000',
-            './src/frontend/portfolioView.js',
-        ],
-        contact: ['webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000', './src/frontend/contact.js'],
+        index: ['webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000', './src/frontend/index.tsx'],
     },
     output: {
         path: path.join(__dirname, 'build'),
@@ -23,23 +17,9 @@ module.exports = {
     module: {
         rules: [
             {
-                enforce: 'pre',
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'eslint-loader',
-                options: {
-                    emitWarning: true,
-                    failOnError: false,
-                    failOnWarning: false,
-                },
-            },
-            {
-                test: /\.js|jsx$/,
+                test: /\.tsx?$/,
                 exclude: /node_modules/,
                 loader: 'babel-loader',
-                options: {
-                    plugins: ['@babel/plugin-transform-runtime'],
-                },
             },
             {
                 test: /\.(sa|sc|c)ss$/,
@@ -47,50 +27,48 @@ module.exports = {
             },
             {
                 test: /\.html$/,
-                loader: 'html-loader',
+                use: [
+                    {
+                        loader: 'html-loader',
+                    },
+                ],
             },
             {
                 test: /\.(png|svg|jpe?g|gif)$/i,
-                loader: 'file-loader',
-                options: {
-                    outputPath: './public/assets/images',
-                },
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: './public/assets/images',
+                        },
+                    },
+                ],
             },
             {
                 test: /\.(pdf|md)$/i,
-                loader: 'file-loader',
-                options: {
-                    outputPath: './public/assets/files',
-                },
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: './public/assets/files',
+                        },
+                    },
+                ],
             },
         ],
     },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js', 'css', '.scss'],
+    },
     plugins: [
+        new ESLintPlugin({
+            context: 'src/frontend',
+            extensions: ['tsx', 'js', 'jsx'],
+        }),
         new HtmlWebPackPlugin({
             template: './src/frontend/pages/index.html',
             filename: './public/index.html',
-            excludeChunks: ['server', 'portfolio', 'portfolio_view', 'contact'],
-        }),
-        new HtmlWebPackPlugin({
-            template: './src/frontend/pages/portfolio.html',
-            filename: './public/portfolio.html',
-            excludeChunks: ['server', 'index', 'portfolio_view', 'contact'],
-        }),
-        new HtmlWebPackPlugin({
-            template: './src/frontend/pages/portfolioView.html',
-            filename: './public/portfolioView.html',
-            excludeChunks: ['server', 'portfolio', 'index', 'contact'],
-        }),
-        new HtmlWebPackPlugin({
-            template: './src/frontend/pages/contact.html',
-            filename: './public/contact.html',
-            excludeChunks: ['server', 'portfolio', 'portfolio_view', 'index'],
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: 'src/frontend/assets/sitefiles/favicon', to: 'public/assets/favicon' },
-                { from: 'src/frontend/styles/animate.min.css', to: 'public/styles' },
-            ],
+            excludeChunks: ['server'],
         }),
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NoEmitOnErrorsPlugin(),
