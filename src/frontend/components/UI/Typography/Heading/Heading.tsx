4903ff466628f243a5b4a15f4683ecdaa011075e
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2021-03-29 00:34:17 +0100

    update: Minor changes

--- src/frontend/components/UI/Typography/Heading/Heading.tsx
+++ src/frontend/components/UI/Typography/Heading/Heading.tsx
@@ -2,28 +2,52 @@ import React from 'react';
 
 import './Heading.scss';
 
+interface Classes {
+    [index: number]: string;
+}
+
 type HeadingProps = {
     importance: number;
     title: string;
-    classes?: string;
+    classes?: Classes[];
 };
 
-export const Heading = ({ importance, title, classes = '' }: HeadingProps): JSX.Element => {
-    classes = classes !== '' ? ' ' + classes : '';
+export const Heading = ({ importance, title, classes = [] }: HeadingProps): JSX.Element => {
+    const isBold = classes.indexOf('bold');
+    const isUnderline = classes.indexOf('underline');
+    const isSpaced = classes.indexOf('spaced');
+    let classStr = '';
+
+    if (isBold > -1) {
+        classes[isBold] = 'h' + importance + '--bold';
+    }
+    if (isUnderline > -1) {
+        classes[isUnderline] = 'h' + importance + '--underline';
+    }
+    if (isSpaced > -1) {
+        classes[isSpaced] = 'h' + importance + '--space-out';
+    }
+
+    if (classes.length !== 0) {
+        for (const style in classes) {
+            classStr = classStr + ' ' + classes[style];
+        }
+    }
+
     switch (importance) {
         case 1:
-            return <h1 className={'h1' + classes}>{title}</h1>;
+            return <h1 className={'h1' + classStr}>{title}</h1>;
         case 2:
-            return <h2 className={'h2' + classes}>{title}</h2>;
+            return <h2 className={'h2' + classStr}>{title}</h2>;
         case 3:
-            return <h3 className={'h3' + classes}>{title}</h3>;
+            return <h3 className={'h3' + classStr}>{title}</h3>;
         case 4:
-            return <h4 className={'h4' + classes}>{title}</h4>;
+            return <h4 className={'h4' + classStr}>{title}</h4>;
         case 5:
-            return <h5 className={'h5' + classes}>{title}</h5>;
+            return <h5 className={'h5' + classStr}>{title}</h5>;
         case 6:
-            return <h6 className={'h6' + classes}>{title}</h6>;
+            return <h6 className={'h6' + classStr}>{title}</h6>;
         default:
-            return <h1 className={'h1' + classes}>{title}</h1>;
+            return <h1 className={'h1' + classStr}>{title}</h1>;
     }
 };
--- src/frontend/containers/UIDesignSystem/UIDesignSystem.tsx
+++ src/frontend/containers/UIDesignSystem/UIDesignSystem.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
 
-import { Header } from '../../components/Header/Header';
 import { DefaultButton } from '../../components/UI/Button/DefaultButton/DefaultButton';
 import { Heading } from '../../components/UI/Typography/Heading/Heading';
 
 export const UIDesignSystem = (): JSX.Element => {
     return (
         <>
-            <Header test={'TEST'} />
             <DefaultButton text={'button'} />
 
-            <Heading importance={1} title={'title'} classes={''} />
-            <Heading importance={2} title={'title'} classes={''} />
-            <Heading importance={3} title={'title'} classes={''} />
-            <Heading importance={4} title={'title'} classes={''} />
+            <Heading importance={1} title={'title'} classes={[]} />
+            <Heading importance={2} title={'title'} classes={[]} />
+            <Heading importance={3} title={'title'} classes={[]} />
+            <Heading importance={4} title={'title'} classes={[]} />
 
-            <Heading importance={1} title={'title'} classes={'h1-bold'} />
-            <Heading importance={2} title={'title'} classes={'h2-bold'} />
-            <Heading importance={3} title={'title'} classes={'h3-bold'} />
-            <Heading importance={4} title={'title'} classes={'h4-bold'} />
+            <Heading importance={1} title={'title'} classes={['bold']} />
+            <Heading importance={2} title={'title'} classes={['bold']} />
+            <Heading importance={3} title={'title'} classes={['bold']} />
+            <Heading importance={4} title={'title'} classes={['bold']} />
 
             <p>Paragraph text.</p>
         </>
