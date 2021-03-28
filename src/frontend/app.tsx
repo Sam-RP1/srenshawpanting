a6522df4b131dd5a36b2b8405ecf0de9aa22db48
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2021-03-29 00:36:12 +0100

    update: Minor changes

--- src/frontend/app.tsx
+++ src/frontend/app.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 // import { Home } from './containers/Home/Home';
+import { Header } from './components/Header/Header';
+import { About } from './components/About/About';
 import { UIDesignSystem } from './containers/UIDesignSystem/UIDesignSystem';
 
 export const App = (): JSX.Element => {
     return (
         <section className='theme theme-dark' data-test='component-app'>
             <BrowserRouter>
+                <Header />
+                <Route path='/about' component={About} />
                 <Route path='/' exact component={UIDesignSystem} />
             </BrowserRouter>
         </section>
