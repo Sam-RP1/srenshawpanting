6d8d882d29618005160df1926e571962a95e8c96
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2021-03-29 00:35:33 +0100

    created Home.tsx

---
+++ src/frontend/containers/Home/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Header } from '../../components/Header/Header';
+
+type HomeProps = {};
+
+export const Home = ({}: HomeProps) => {
+    return <Header test={'TEST'} />;
+};
