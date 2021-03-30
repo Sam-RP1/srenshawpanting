767c84a43268e01440a7acc22e598e4c022db657
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2021-03-30 20:33:03 +0100

    update: created links and icons arrays containing objects for use in props across project. Also moved fontawesome imports to be here as well.

--- src/frontend/app.tsx
+++ src/frontend/app.tsx
@@ -1,18 +1,74 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, NavLink, Route } from 'react-router-dom';
 
-// import { Home } from './containers/Home/Home';
 import { Header } from './components/Header/Header';
+import { Home } from './containers/Home/Home';
 import { About } from './components/About/About';
-import { UIDesignSystem } from './containers/UIDesignSystem/UIDesignSystem';
+import { Footer } from './components/Footer/Footer';
+// import { UIDesignSystem } from './containers/UIDesignSystem/UIDesignSystem';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChessRook } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedinIn, faInstagram, faGithub, faYoutube } from '@fortawesome/free-brands-svg-icons';
 
 export const App = (): JSX.Element => {
+    const links = [
+        {
+            id: 'about',
+            url: (
+                <NavLink key={'about-link'} to='/about'>
+                    About
+                </NavLink>
+            ),
+        },
+        {
+            id: 'portfolio',
+            url: (
+                <NavLink key={'portfolio-link'} to='/portfolio'>
+                    Portfolio
+                </NavLink>
+            ),
+        },
+        {
+            id: 'cv',
+            url: (
+                <NavLink key={'cv-link'} to='/cv'>
+                    CV
+                </NavLink>
+            ),
+        },
+        {
+            id: 'connect',
+            url: (
+                <NavLink key={'connect-link'} to='/connect'>
+                    Connect
+                </NavLink>
+            ),
+        },
+    ];
+    const icons = [
+        { id: 'github', url: 'https://github.com/Sam-RP1', icon: <FontAwesomeIcon icon={faGithub} /> },
+        {
+            id: 'linkedin',
+            url: 'https://www.linkedin.com/in/samuel-renshaw-panting/',
+            icon: <FontAwesomeIcon icon={faLinkedinIn} />,
+        },
+        { id: 'chess', url: 'https://www.chess.com/member/samuelr-p', icon: <FontAwesomeIcon icon={faChessRook} /> },
+        {
+            id: 'youtube',
+            url: 'https://www.youtube.com/user/TheWarpCentral',
+            icon: <FontAwesomeIcon icon={faYoutube} />,
+        },
+        { id: 'instagram', url: 'https://www.instagram.com/sxm_rp/', icon: <FontAwesomeIcon icon={faInstagram} /> },
+    ];
+
     return (
         <section className='theme theme-dark' data-test='component-app'>
             <BrowserRouter>
                 <Header />
                 <Route path='/about' component={About} />
-                <Route path='/' exact component={UIDesignSystem} />
+                <Route path='/' exact component={Home} />
+                <Footer links={links} icons={icons} />
             </BrowserRouter>
         </section>
     );
