a0449553dbd3067c95c9f158e4fbdd8c1dfd4c87
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2021-04-02 20:15:58 +0100

    update: imported Connect.tsx presentational component

---
+++ src/frontend/containers/Connect/Connect.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Connect as ConnectCmpnt } from '../../components/Connect/Connect';
+
+import { icons } from '../../lib/icons';
+
+// type ConnectProps = {};
+
+export const Connect = (): JSX.Element => {
+    return <ConnectCmpnt icons={icons} />;
+};
