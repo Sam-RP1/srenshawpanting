959e3bc53860a9f6892e5d93aba166dd11f83d6f
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2021-04-02 20:16:20 +0100

    created interfaces,tsx

---
+++ src/frontend/lib/interfaces.ts
@@ -0,0 +1,14 @@
+export interface Icon {
+    id: string;
+    url: string;
+    icon: JSX.Element;
+    tooltip: string;
+}
+export interface Icons extends Array<Icon> {}
+
+export interface Link {
+    id: string;
+    url: JSX.Element;
+    tooltip?: string;
+}
+export interface Links extends Array<Link> {}
