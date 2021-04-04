14f4c92fc160bc2b2ff3d1d599b7c8d7907ead95
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2021-04-04 18:04:44 +0100

    created Portfolio.tsx container

---
+++ src/frontend/containers/Portfolio/Portfolio.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Portfolio as PortfolioCmpnt } from '../../components/Portfolio/Portfolio';
+
+// type ConnectProps = {};
+
+export const Portfolio = (): JSX.Element => {
+    return <PortfolioCmpnt />;
+};
