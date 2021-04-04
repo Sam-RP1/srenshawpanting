5fc194327db27b167e85e57bf9f4f19021ec1f13
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2021-04-04 18:03:08 +0100

    update: added and updated imports

--- src/frontend/app.tsx
+++ src/frontend/app.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
+import { ScrollToTop } from './hooks/useScrollToTop';
 import { Header } from './components/Header/Header';
 import { Home } from './containers/Home/Home';
-import { About } from './components/About/About';
+import { About } from './containers/About/About';
+import { Portfolio } from './containers/Portfolio/Portfolio';
 import { Connect } from './containers/Connect/Connect';
 import { Footer } from './components/Footer/Footer';
+import { Background } from './components/Background/Background';
 // import { UIDesignSystem } from './containers/UIDesignSystem/UIDesignSystem';
 
 import { icons } from './lib/icons';
@@ -15,12 +18,15 @@ export const App = (): JSX.Element => {
     return (
         <section className='theme theme-dark' data-test='component-app'>
             <BrowserRouter>
+                <ScrollToTop />
                 <Header />
                 <Route path='/connect' component={Connect} />
+                <Route path='/portfolio' component={Portfolio} />
                 <Route path='/about' component={About} />
                 <Route path='/' exact component={Home} />
                 <Footer icons={icons} links={links} />
             </BrowserRouter>
+            <Background />
         </section>
     );
 };
