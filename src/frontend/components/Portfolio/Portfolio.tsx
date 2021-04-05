ab481a06f2cd11b921cf2117d68fa7a3b94a4b4f
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2021-04-05 20:55:54 +0100

    update: created a temporary method for creating portfolio tiles

--- src/frontend/components/Portfolio/Portfolio.tsx
+++ src/frontend/components/Portfolio/Portfolio.tsx
@@ -13,15 +13,67 @@ type PortfolioProps = {
 };
 
 export const Portfolio = ({ links, icons }: PortfolioProps): JSX.Element => {
-    // const linkElems = links.map((link, i) => {
-    //     const isDivided = i <= links.length - 2 ? <div>|</div> : null;
-    //     return (
-    //         <React.Fragment key={link.id + '-footer'}>
-    //             {link.url}
-    //             {isDivided}
-    //         </React.Fragment>
-    //     );
-    // });
+    const portfolioItems = [
+        {
+            id: 'deduction',
+            title: 'Deduction',
+            description: 'Multiplayer Web Game',
+        },
+        {
+            id: 'peerreviewportal',
+            title: 'Peer Review Portal',
+            description: 'Web Application',
+        },
+        {
+            id: 'letterdetonator',
+            title: 'Letter Detonator',
+            description: 'Web Game',
+        },
+        {
+            id: 'karensbeautybox',
+            title: 'Karens Beauty Box',
+            description: 'Website',
+        },
+        {
+            id: 'pudhub',
+            title: 'P.U.D Hub',
+            description: 'Web Application',
+        },
+        {
+            id: 'fred',
+            title: 'FRED',
+            description: 'Website',
+        },
+        {
+            id: 'robotics',
+            title: 'Robotics',
+            description: 'Univeristy Coursework',
+        },
+        {
+            id: 'distibutedsystems',
+            title: 'Distributed Systems',
+            description: 'Univeristy Research',
+        },
+        {
+            id: 'literaturereview',
+            title: 'Mitigating Data Exposure',
+            description: 'Univeristy Literature Review',
+        },
+    ];
+
+    const portfolioTiles = portfolioItems.map((item) => {
+        return (
+            <div key={item.id} id={item.id} className='portfolio__tile'>
+                <div className='portfolio__tile__preview'>
+                    <div className='portfolio__tile__preview__filter'></div>
+                </div>
+                <div className='portfolio__tile__label'>
+                    <Heading importance={5} title={item.title} classes={['spaced']} />
+                    <p>{item.description}</p>
+                </div>
+            </div>
+        );
+    });
 
     return (
         <Section classes={['default', 'center-center', 'portfolio']}>
@@ -29,27 +81,7 @@ export const Portfolio = ({ links, icons }: PortfolioProps): JSX.Element => {
                 <>
                     <Heading importance={3} title={'Portfolio'} classes={['uppercase', 'spaced', 'underline']} />
 
-                    <div className='portfolio__content__container'>
-                        <div id='deduction' className='portfolio__tile'>
-                            <div className='portfolio__tile__preview'>
-                                <div className='portfolio__tile__preview__filter'></div>
-                            </div>
-                            <div className='portfolio__tile__label'>
-                                <Heading importance={5} title={'Deduction'} classes={['spaced']} />
-                                <p>Multiplayer Web Game</p>
-                            </div>
-                        </div>
-
-                        <div id='peerreviewportal' className='portfolio__tile'>
-                            <div className='portfolio__tile__preview'>
-                                <div className='portfolio__tile__preview__filter'></div>
-                            </div>
-                            <div className='portfolio__tile__label'>
-                                <Heading importance={5} title={'Peer Review Portal'} classes={['spaced']} />
-                                <p>Web Application</p>
-                            </div>
-                        </div>
-                    </div>
+                    <div className='portfolio__content__container'>{portfolioTiles}</div>
                 </>
             </Container>
         </Section>
