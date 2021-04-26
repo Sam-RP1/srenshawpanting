1920ffec5da2636c509c4ec68b5c920d157d20e0
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2021-04-26 22:07:49 +0100

    update: restructured content

--- src/frontend/components/Home/Home.tsx
+++ src/frontend/components/Home/Home.tsx
@@ -6,22 +6,43 @@ import { Container } from '../UI/Container/Container';
 
 import './Home.scss';
 
-import propic from '../../assets/pictures/propic.jpg';
+// import propic from '../../assets/pictures/propic.jpg';
 
 export const Home = (): JSX.Element => {
+    // move to container
+    const scrollTo = () => {
+        const wHeight = window.innerHeight;
+        console.log('SCROLL');
+        window.scrollTo({ top: wHeight, behavior: 'smooth' });
+    };
+
     return (
-        <Section classes={['default', 'end-center', 'home']}>
-            <Container classes={'home__content'}>
-                <>
-                    <article className='home__content__hero-text'>
-                        <Heading importance={3} title={'Samuel Renshaw-Panting'} classes={['spaced', 'underline']} />
-                        <p className=''>Software Engineering Graduate & Web Developer</p>
-                    </article>
-                    {/* <div className='home__content__pic'>
-                        <img src={propic} />
-                    </div> */}
-                </>
-            </Container>
-        </Section>
+        <>
+            <Section classes={['default', 'end-center']}>
+                <Container classes={'hero'}>
+                    <>
+                        <div className='hero__text'>
+                            <Heading
+                                importance={4}
+                                title={'Samuel Renshaw-Panting'}
+                                classes={['spaced', 'underline']}
+                            />
+                            <p className=''>Software Engineering Graduate & Web Developer</p>
+                        </div>
+                        <div className='hero__next'>
+                            <div className='hero__next__item' onClick={() => scrollTo()}>
+                                <p className='hero__next__item__label'>next()</p>
+                                <span className='hero__next__item__arrow-down'></span>
+                            </div>
+                        </div>
+                    </>
+                </Container>
+            </Section>
+            <Section classes={['default', 'portfolio']}>
+                <Container classes={'portfolio__content'}>
+                    <div></div>
+                </Container>
+            </Section>
+        </>
     );
 };
