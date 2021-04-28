7be82f2be43a229fefbd8bcaecacc593bc23ff87
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2021-04-28 22:35:42 +0100

    update: added sitePages array containing objects for each page tile

--- src/frontend/containers/Home/Home.tsx
+++ src/frontend/containers/Home/Home.tsx
@@ -1,9 +1,67 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDoubleRight, faUserCircle, faFileAlt, faPlug, faMeteor } from '@fortawesome/free-solid-svg-icons';
+
 import { Home as HomeCmpnt } from '../../components/Home/Home';
 
-// type HomeProps = {};
-
 export const Home = (): JSX.Element => {
-    return <HomeCmpnt />;
+    const learnMoreIcon = <FontAwesomeIcon icon={faAngleDoubleRight} />;
+
+    const sitePages = [
+        {
+            hero: <FontAwesomeIcon icon={faUserCircle} />,
+            id: 'about',
+            megaTitle: 'About',
+            title: 'About',
+            route: '/about',
+            buttons: [
+                {
+                    id: 'about-more-btn',
+                    icon: learnMoreIcon,
+                },
+            ],
+        },
+        {
+            hero: <FontAwesomeIcon icon={faMeteor} />,
+            id: 'portfolio',
+            megaTitle: 'Portfolio',
+            title: 'Portfolio',
+            route: '/portfolio',
+            buttons: [
+                {
+                    id: 'portfolio-more-btn',
+                    icon: learnMoreIcon,
+                },
+            ],
+        },
+        {
+            hero: <FontAwesomeIcon icon={faFileAlt} />,
+            id: 'cv',
+            megaTitle: 'Curriculum Vitae',
+            title: 'CV',
+            route: '/cv',
+            buttons: [
+                {
+                    id: 'cv-more-btn',
+                    icon: learnMoreIcon,
+                },
+            ],
+        },
+        {
+            hero: <FontAwesomeIcon icon={faPlug} />,
+            id: 'connect',
+            megaTitle: 'Connect',
+            title: 'Connect',
+            route: '/connect',
+            buttons: [
+                {
+                    id: 'connect-more-btn',
+                    icon: learnMoreIcon,
+                },
+            ],
+        },
+    ];
+
+    return <HomeCmpnt sitePages={sitePages} />;
 };
