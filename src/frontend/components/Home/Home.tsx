326ee8c55822c554c1ed6591370fa17738e99d76
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2021-04-28 22:35:05 +0100

    update: created tiles for site pages

--- src/frontend/components/Home/Home.tsx
+++ src/frontend/components/Home/Home.tsx
@@ -1,24 +1,59 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import { Heading } from '../UI/Typography/Heading/Heading';
 import { Section } from '../UI/Section/Section';
 import { Container } from '../UI/Container/Container';
 
+import { Icon, Icons } from '../../lib/interfaces';
+
 import './Home.scss';
 
-// import propic from '../../assets/pictures/propic.jpg';
+type HomeProps = {
+    sitePages: {
+        hero: Icon;
+        megaTitle: string;
+        id: string;
+        title: string;
+        route: string;
+        buttons: Icons;
+    }[];
+};
 
-export const Home = (): JSX.Element => {
-    // move to container
+export const Home = ({ sitePages }: HomeProps): JSX.Element => {
     const scrollTo = () => {
-        const wHeight = window.innerHeight;
-        console.log('SCROLL');
+        const wHeight = window.innerHeight - 172;
+        // Scroll the height of the where next parent elem
         window.scrollTo({ top: wHeight, behavior: 'smooth' });
     };
 
+    // On mobile have padding turned off on the parent elem so the blur/bg is 100% width then on desktop have it so that there is 1rem padding
+
+    const content = sitePages.map((page) => {
+        return (
+            <div key={page.id} className='site-pages__tile '>
+                <div className='site-pages__tile__content container'>
+                    <div className='site-pages__tile__mega-title'>
+                        <h1>{page.megaTitle}</h1>
+                    </div>
+                    <div className='site-pages__tile__hero'>{page.hero}</div>
+
+                    {page.buttons.map((btn) => {
+                        return (
+                            <NavLink key={page.id + page.route} to={page.route} className='site-pages__tile__button'>
+                                <h3>{page.title}</h3>
+                                <div className='icon-wrapper'>{btn.icon}</div>
+                            </NavLink>
+                        );
+                    })}
+                </div>
+            </div>
+        );
+    });
+
     return (
         <>
-            <Section classes={['default', 'end-center']}>
+            <Section classes={['top', 'end-center']}>
                 <Container classes={'hero'}>
                     <>
                         <div className='hero__text'>
@@ -31,16 +66,16 @@ export const Home = (): JSX.Element => {
                         </div>
                         <div className='hero__next'>
                             <div className='hero__next__item' onClick={() => scrollTo()}>
-                                <p className='hero__next__item__label'>next()</p>
+                                <p className='hero__next__item__label'>Where next?</p>
                                 <span className='hero__next__item__arrow-down'></span>
                             </div>
                         </div>
                     </>
                 </Container>
             </Section>
-            <Section classes={['default', 'portfolio']}>
-                <Container classes={'portfolio__content'}>
-                    <div></div>
+            <Section classes={['default', 'center-center']}>
+                <Container classes={'site-pages'}>
+                    <>{content}</>
                 </Container>
             </Section>
         </>
