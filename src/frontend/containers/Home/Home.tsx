3d44d2b4785cddd627d45e9aafa83c61eea80be1
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2021-04-29 22:11:11 +0100

    update: updated the sitePages array of objects, removed button property and renamed hero to icon

--- src/frontend/containers/Home/Home.tsx
+++ src/frontend/containers/Home/Home.tsx
@@ -6,62 +6,38 @@ import { faAngleDoubleRight, faUserCircle, faFileAlt, faPlug, faMeteor } from '@
 import { Home as HomeCmpnt } from '../../components/Home/Home';
 
 export const Home = (): JSX.Element => {
-    const learnMoreIcon = <FontAwesomeIcon icon={faAngleDoubleRight} />;
+    const arrowsIcon = <FontAwesomeIcon icon={faAngleDoubleRight} />;
 
     const sitePages = [
         {
-            hero: <FontAwesomeIcon icon={faUserCircle} />,
+            icon: <FontAwesomeIcon icon={faUserCircle} />,
             id: 'about',
             megaTitle: 'About',
             title: 'About',
             route: '/about',
-            buttons: [
-                {
-                    id: 'about-more-btn',
-                    icon: learnMoreIcon,
-                },
-            ],
         },
         {
-            hero: <FontAwesomeIcon icon={faMeteor} />,
+            icon: <FontAwesomeIcon icon={faMeteor} />,
             id: 'portfolio',
             megaTitle: 'Portfolio',
             title: 'Portfolio',
             route: '/portfolio',
-            buttons: [
-                {
-                    id: 'portfolio-more-btn',
-                    icon: learnMoreIcon,
-                },
-            ],
         },
         {
-            hero: <FontAwesomeIcon icon={faFileAlt} />,
+            icon: <FontAwesomeIcon icon={faFileAlt} />,
             id: 'cv',
             megaTitle: 'Curriculum Vitae',
             title: 'CV',
             route: '/cv',
-            buttons: [
-                {
-                    id: 'cv-more-btn',
-                    icon: learnMoreIcon,
-                },
-            ],
         },
         {
-            hero: <FontAwesomeIcon icon={faPlug} />,
+            icon: <FontAwesomeIcon icon={faPlug} />,
             id: 'connect',
             megaTitle: 'Connect',
             title: 'Connect',
             route: '/connect',
-            buttons: [
-                {
-                    id: 'connect-more-btn',
-                    icon: learnMoreIcon,
-                },
-            ],
         },
     ];
 
-    return <HomeCmpnt sitePages={sitePages} />;
+    return <HomeCmpnt sitePages={sitePages} arrowsIcon={arrowsIcon} />;
 };
