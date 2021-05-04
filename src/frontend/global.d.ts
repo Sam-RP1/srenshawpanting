d5cb8959a83991277b443c234fdd0d99ea341f6e
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2021-05-04 23:40:21 +0100

    update: changed png declaration to any for type checking

--- src/frontend/global.d.ts
+++ src/frontend/global.d.ts
@@ -1,4 +1,7 @@
 declare module '*.scss';
-declare module '*.png';
+declare module '*.png' {
+    const value: any;
+    export = value;
+}
 declare module '*.jpg';
 declare module 'animejs/lib/anime.es.js';
