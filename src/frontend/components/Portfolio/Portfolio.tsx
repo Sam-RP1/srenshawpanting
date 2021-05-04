a68365c051d80996501bd9c5a47f7ac58c2fd286
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2021-05-04 23:42:49 +0100

    update: experimenting and prototyping new approach to displaying portfolio items, horribly done needs optimising and better structure

--- src/frontend/components/Portfolio/Portfolio.tsx
+++ src/frontend/components/Portfolio/Portfolio.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
+import { gsap } from 'gsap';
 
 import { Heading } from '../UI/Typography/Heading/Heading';
 import { Section } from '../UI/Section/Section';
@@ -7,6 +9,7 @@ import { Container } from '../UI/Container/Container';
 import { Icons } from '../../lib/interfaces';
 
 import './Portfolio.scss';
+// Could add a next and/or prev button to the timeline to jump between sections/segements using state and stuff when it is its own component
 
 type PortfolioProps = {
     professionalItems: {
@@ -16,6 +19,7 @@ type PortfolioProps = {
         description: string;
         techStack: string;
         buttons: Icons;
+        img: string;
     }[];
     academicItems: {
         id: string;
@@ -28,88 +32,154 @@ type PortfolioProps = {
 };
 
 export const Portfolio = ({ professionalItems, academicItems }: PortfolioProps): JSX.Element => {
-    const professional = professionalItems.map((item) => {
+    const professional = professionalItems.map((item, i) => {
+        const img = item.img;
         return (
-            <div key={item.id} className='portfolio__item'>
-                <Container classes='portfolio__item__content'>
-                    <>
-                        <div className='portfolio__item__content__title'>
-                            <Heading importance={4} title={item.title} classes={['spaced']} />
-                        </div>
+            <div key={item.id} className='portfolio__panel'>
+                <div onClick={() => console.log('OPEN PROJECT')} className='portfolio__panel__container'>
+                    <div className='portfolio__panel__image' style={{ backgroundImage: 'url(' + img + ')' }}></div>
+                    <div className='portfolio__panel__glare'>
+                        <div className='portfolio__panel__glare__inner'></div>
+                    </div>
 
-                        <div className='portfolio__item__content__description'>
-                            <p>
-                                {item.description} {item.techStack}
-                            </p>
+                    <div className='portfolio__panel__content'>
+                        <div className='portfolio__panel__info'>
+                            <div className='portfolio__panel__title'>
+                                <h5>{item.title}</h5>
+                            </div>
+                            <div className='portfolio__panel__detail'>
+                                <p className='portfolio__panel__int'>[{'0' + i}]</p>
+                                <p className='portfolio__panel__context'>{item.description}</p>
+                            </div>
                         </div>
-                        <div className='portfolio__item__content__buttons'>
-                            {item.buttons.map((btn) => {
-                                return (
-                                    <a key={btn.id} href={btn.url} target='_blank' rel='noreferrer'>
-                                        <div className=''>{btn.label}</div>
-                                        <div className='icon-wrapper'>{btn.icon}</div>
-                                    </a>
-                                );
-                            })}
-                        </div>
-                    </>
-                </Container>
+                        {item.buttons.map((btn) => {
+                            return (
+                                <div key={btn.id} className='icon-wrapper'>
+                                    {btn.icon}
+                                </div>
+                            );
+                        })}
+                    </div>
+                </div>
             </div>
         );
     });
 
-    const academic = academicItems.map((item) => {
+    const academic = academicItems.map((item, i) => {
+        const img = item.img;
         return (
-            <div key={item.id} className='portfolio__item'>
-                <Container classes='portfolio__item__content indent'>
-                    <>
-                        <div className='portfolio__item__content__title'>
-                            <Heading importance={4} title={item.title} classes={['spaced']} />
-                        </div>
+            <div key={item.id} className='portfolio__panel'>
+                <div onClick={() => console.log('OPEN PROJECT')} className='portfolio__panel__container'>
+                    <div className='portfolio__panel__image' style={{ backgroundImage: 'url(' + img + ')' }}></div>
+                    <div className='portfolio__panel__glare'>
+                        <div className='portfolio__panel__glare__inner'></div>
+                    </div>
 
-                        <div className='portfolio__item__content__description'>
-                            <p>
-                                {item.description} {item.techStack}
-                            </p>
+                    <div className='portfolio__panel__content'>
+                        <div className='portfolio__panel__info'>
+                            <div className='portfolio__panel__title'>
+                                <h5>{item.title}</h5>
+                            </div>
+                            <div className='portfolio__panel__detail'>
+                                <p className='portfolio__panel__int'>[{'0' + i}]</p>
+                                <p className='portfolio__panel__context'>{item.description}</p>
+                            </div>
                         </div>
-                        <div className='portfolio__item__content__buttons'>
-                            {item.buttons.map((btn) => {
-                                return (
-                                    <a key={btn.id} href={btn.url} target='_blank' rel='noreferrer'>
-                                        <div className=''>{btn.label}</div>
-                                        <div className='icon-wrapper'>{btn.icon}</div>
-                                    </a>
-                                );
-                            })}
-                        </div>
-                    </>
-                </Container>
+                        {item.buttons.map((btn) => {
+                            return (
+                                <div key={btn.id} className='icon-wrapper'>
+                                    {btn.icon}
+                                </div>
+                            );
+                        })}
+                    </div>
+                </div>
             </div>
         );
     });
+
+    useEffect(() => {
+        const ANGLE = 45;
+        // disable on small screen devices
+
+        const onMouseEnter = (elem) => {
+            elem.style.transition = `all 200ms cubic-bezier(0.3, 0.98, 0.52, 0.99)`;
+            setTimeout(() => {
+                elem.style.transition = 'initial';
+            }, 201);
+        };
+
+        const onMouseLeave = (elem) => {
+            elem.style.transition = `all 200ms cubic-bezier(0.3, 0.98, 0.52, 0.99)`;
+            elem.style.transform = `perspective(700px)
+                           rotateX(0deg)
+                           rotateY(0deg)
+                           rotateZ(0deg)`;
+            setTimeout(() => {
+                elem.style.transition = 'initial';
+            }, 201);
+        };
+
+        const onMouseMove = (e, parentElem, childElem) => {
+            const w = parentElem.clientWidth;
+            const h = parentElem.clientHeight;
+            const y = ((e.offsetX - w * 0.5) / w) * ANGLE;
+            const x = ((e.offsetY - h * 0.5) / h) * ANGLE;
+            gsap.to(childElem, {
+                duration: 0,
+                perspective: 700,
+                rotateX: x,
+                rotateY: y,
+                rotateZ: 0,
+                scale: 1.05,
+            });
+            // scale??
+        };
+
+        // make into non-anonymous functions
+        document.querySelectorAll('.portfolio__panel').forEach((panel) => {
+            const panelContainer = panel.querySelector('.portfolio__panel__container');
+            panel.addEventListener('mouseenter', () => onMouseEnter(panelContainer));
+
+            panel.addEventListener('mouseout', () => onMouseLeave(panelContainer));
+
+            panel.addEventListener('mousemove', (e) => onMouseMove(e, panel, panelContainer));
+        });
+
+        return () => {
+            document.querySelectorAll('.portfolio__panel').forEach((elem) => {
+                // elem.removeEventListener();
+            });
+        };
+    }, []);
 
     return (
-        <Section classes={['center-center', 'portfolio']}>
-            <div className='portfolio__content'>
-                <Container>
-                    <Heading importance={3} title={'Portfolio'} classes={['spaced']} open={true} />
-                </Container>
+        <Section classes={['top', 'center-center']}>
+            <Container classes='portfolio'>
+                <>
+                    <div className='portfolio__intro'>
+                        <div className='timeline'></div>
+                        <div className='portfolio__content-wrapper'>
+                            <div className='timeline__title-container'>
+                                <h3 className='timeline__title'>Portfolio{'/>'}</h3>
+                            </div>
+                            {/* <p>Select projects spanning both my professional and educational careers.</p> */}
+                            {professional}
+                        </div>
+                    </div>
 
-                <div className='portfolio__content__container'>{professional}</div>
+                    <div className='portfolio__item'>
+                        <div className='timeline'></div>
+                        <div className='portfolio__content-wrapper'>
+                            <div className='timeline__title-container'>
+                                <h3 className='timeline__title'>Portfolio</h3>
+                            </div>
 
-                <Container>
-                    <Heading importance={3} title={'Academics'} classes={['spaced', 'indent']} open={true} />
-                </Container>
-
-                <div className='portfolio__content__container'>{academic}</div>
-
-                <Container>
-                    <>
-                        <Heading importance={3} title={'Academics'} classes={['spaced', 'indent']} close={true} />
-                        <Heading importance={3} title={'Portfolio'} classes={['spaced']} close={true} />
-                    </>
-                </Container>
-            </div>
+                            {academic}
+                        </div>
+                    </div>
+                </>
+            </Container>
         </Section>
     );
 };
--- src/frontend/containers/Portfolio/Portfolio.tsx
+++ src/frontend/containers/Portfolio/Portfolio.tsx
@@ -6,6 +6,15 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 import { Portfolio as PortfolioCmpnt } from '../../components/Portfolio/Portfolio';
 
+// eslint-disable-next-line
+const deductionPrev = require('../../assets/portfolio/deduction/img/deduction-min.png');
+// eslint-disable-next-line
+const letterDetonatorPrev = require('../../assets/portfolio/letter-detonator/img/letter-detonator-min.png');
+// eslint-disable-next-line
+const karensBeautyBoxPrev = require('../../assets/portfolio/karensbeautybox/img/karensbeautybox-min.png');
+// eslint-disable-next-line
+const peerReviewPortalPrev = require('../../assets/portfolio/peer-review-portal/img/prp-min.png');
+
 export const Portfolio = (): JSX.Element => {
     const gitHubIcon = <FontAwesomeIcon icon={faGithub} />;
     const externalLinkIcon = <FontAwesomeIcon icon={faExternalLinkSquareAlt} />;
@@ -16,22 +25,20 @@ export const Portfolio = (): JSX.Element => {
             id: 'deduction',
             title: 'Deduction',
             created: '2020',
-            description:
-                'Deduction is a real-time multiplayer web game in which players are split into two teams and face off against each other!',
-            techStack: 'Built using React, Redux, SCSS, Express, Socket.io, MariaDB, Webpack and hosted using AWS.',
+            description: 'Multiplayer web game',
             buttons: [
-                {
-                    id: 'deduction-visit-btn',
-                    url: 'http://www.thedeductiongame.com/',
-                    icon: externalLinkIcon,
-                    label: 'Visit Deduction',
-                },
-                {
-                    id: 'deduction-github-btn',
-                    url: 'https://github.com/Sam-RP1/deduction',
-                    icon: gitHubIcon,
-                    label: 'View on GitHub',
-                },
+                // {
+                //     id: 'deduction-visit-btn',
+                //     url: 'http://www.thedeductiongame.com/',
+                //     icon: externalLinkIcon,
+                //     label: 'Visit Deduction',
+                // },
+                // {
+                //     id: 'deduction-github-btn',
+                //     url: 'https://github.com/Sam-RP1/deduction',
+                //     icon: gitHubIcon,
+                //     label: 'View on GitHub',
+                // },
                 {
                     id: 'deduction-more-btn',
                     url: '#',
@@ -39,27 +46,26 @@ export const Portfolio = (): JSX.Element => {
                     label: 'Learn More',
                 },
             ],
+            img: deductionPrev,
         },
         {
             id: 'letterdetonator',
             title: 'Letter Detonator',
             created: '2020',
-            description:
-                'Letter Detonator, a web game where players type to survive. Players achieve high scores by typing out the approaching letters causing them to detonate and explode.',
-            techStack: 'Built using HTML Canvas, SCSS, JS, React, Local Storage, Webpack and hosted using AWS.',
+            description: 'Web game',
             buttons: [
-                {
-                    id: 'letterdetonator-visit-btn',
-                    url: 'http://letterdetonator.co.uk/',
-                    icon: externalLinkIcon,
-                    label: 'Visit Letter Detonator',
-                },
-                {
-                    id: 'letterdetonator-github-btn',
-                    url: 'https://github.com/Sam-RP1/letter-detonator',
-                    icon: gitHubIcon,
-                    label: 'View on GitHub',
-                },
+                // {
+                //     id: 'letterdetonator-visit-btn',
+                //     url: 'http://letterdetonator.co.uk/',
+                //     icon: externalLinkIcon,
+                //     label: 'Visit Letter Detonator',
+                // },
+                // {
+                //     id: 'letterdetonator-github-btn',
+                //     url: 'https://github.com/Sam-RP1/letter-detonator',
+                //     icon: gitHubIcon,
+                //     label: 'View on GitHub',
+                // },
                 {
                     id: 'letterdetonator-more-btn',
                     url: '#',
@@ -67,21 +73,20 @@ export const Portfolio = (): JSX.Element => {
                     label: 'Learn More',
                 },
             ],
+            img: letterDetonatorPrev,
         },
         {
             id: 'karensbeautybox',
             title: 'Karens Beauty Box',
             created: '2020',
-            description:
-                'Karens Beauty Box is a single-page website with a refined design that features a moving gradient background alongside bespoke minimalistic icons and a logo.',
-            techStack: 'HTML, SCSS, JS, React, Webpack and hosted using AWS.',
+            description: 'Client Website',
             buttons: [
-                {
-                    id: 'karensbeautybox-visit-btn',
-                    url: 'http://karensbeautybox.co.uk/',
-                    icon: externalLinkIcon,
-                    label: 'Visit Karens Beauty Box',
-                },
+                // {
+                //     id: 'karensbeautybox-visit-btn',
+                //     url: 'http://karensbeautybox.co.uk/',
+                //     icon: externalLinkIcon,
+                //     label: 'Visit Karens Beauty Box',
+                // },
                 {
                     id: 'karensbeautybox-more-btn',
                     url: '#',
@@ -89,21 +94,20 @@ export const Portfolio = (): JSX.Element => {
                     label: 'Learn More',
                 },
             ],
+            img: karensBeautyBoxPrev,
         },
         {
             id: 'peerreviewportal',
             title: 'Peer Review Portal',
             created: '2019',
-            description:
-                'Peer Review Portal is a web application that streamlines and improves the peer and meta review process. It encompasses the setting, collecting and meta-reviewing of work for assignments by teachers and the submitting, peer-reviewing of work and accessing of feedback for assignments by students.',
-            techStack: 'Built using JSX, CSS, React, Babel, Express and MariaDB.',
+            description: 'Web application',
             buttons: [
-                {
-                    id: 'peerreviewportal-report-btn',
-                    url: '#',
-                    icon: externalLinkIcon,
-                    label: 'View Report',
-                },
+                // {
+                //     id: 'peerreviewportal-report-btn',
+                //     url: '#',
+                //     icon: externalLinkIcon,
+                //     label: 'View Report',
+                // },
                 {
                     id: 'peerreviewportal-more-btn',
                     url: '#',
@@ -111,6 +115,7 @@ export const Portfolio = (): JSX.Element => {
                     label: 'Learn More',
                 },
             ],
+            img: peerReviewPortalPrev,
         },
     ];
 
@@ -119,22 +124,20 @@ export const Portfolio = (): JSX.Element => {
             id: 'robotics',
             title: 'Robotics',
             created: '2019',
-            description:
-                'A project with the aim to improve the performance of a mobile robot`s object manipulation and sorting, based on colour, in a simulated scenario of an industrial environment.',
-            techStack: 'Created using V-REP, Lua Script and Kuka YouBot.',
+            description: 'V-REP Simulation',
             buttons: [
-                {
-                    id: 'robotics-report-btn',
-                    url: '#',
-                    icon: externalLinkIcon,
-                    label: 'View Report',
-                },
-                {
-                    id: 'robotics-github-btn',
-                    url: '#',
-                    icon: gitHubIcon,
-                    label: 'View on GitHub',
-                },
+                // {
+                //     id: 'robotics-report-btn',
+                //     url: '#',
+                //     icon: externalLinkIcon,
+                //     label: 'View Report',
+                // },
+                // {
+                //     id: 'robotics-github-btn',
+                //     url: '#',
+                //     icon: gitHubIcon,
+                //     label: 'View on GitHub',
+                // },
                 {
                     id: 'robotics-more-btn',
                     url: '#',
@@ -147,16 +150,15 @@ export const Portfolio = (): JSX.Element => {
             id: 'pudhub',
             title: 'P.U.D Hub',
             created: '2018',
-            description:
-                'Portsmouth Unattended Displays (P.U.D) Hub is a web application that allows for the full creation, customisation and management of a network of unattended displays from a single dashboard.',
+            description: 'Web application',
             techStack: 'Built using HTML, CSS, JS, Express and MariaDB.',
             buttons: [
-                {
-                    id: 'pudhub-github-btn',
-                    url: '#',
-                    icon: gitHubIcon,
-                    label: 'View on GitHub',
-                },
+                // {
+                //     id: 'pudhub-github-btn',
+                //     url: '#',
+                //     icon: gitHubIcon,
+                //     label: 'View on GitHub',
+                // },
                 {
                     id: 'pudhub-more-btn',
                     url: '#',
@@ -169,14 +171,13 @@ export const Portfolio = (): JSX.Element => {
             id: 'distibutedsystems',
             title: 'Distributed Systems',
             created: '2019',
-            description:
-                'A paper in which methods to detect and mitigate DDOS attacks are researched, compared and evaluated for their suitability to be used by IoT temperature sensing devices as a DDOS security mechanism.',
+            description: 'Research Paper',
             techStack: 'Experiment conducted using Node.js, Raspberry Pi 3B+ and a Computer Cluster (7 Nodes).',
             buttons: [
                 {
                     id: 'distibutedsystems-report-btn',
                     url: '#',
-                    icon: externalLinkIcon,
+                    icon: learnMoreIcon,
                     label: 'View Research',
                 },
             ],
@@ -185,14 +186,13 @@ export const Portfolio = (): JSX.Element => {
             id: 'literaturereview',
             title: 'Mitigating Data Exposure',
             created: '2017',
-            description:
-                'A literature review exploring the topic of Mitigating Corporate Information Exposure on the Web with a specific focus on Bring Your Own Device.',
+            description: 'Literature Review',
             techStack: '',
             buttons: [
                 {
                     id: 'literaturereview-report-btn',
                     url: '#',
-                    icon: externalLinkIcon,
+                    icon: learnMoreIcon,
                     label: 'View Literature Review',
                 },
             ],
