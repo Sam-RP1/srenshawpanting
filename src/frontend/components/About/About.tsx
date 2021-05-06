74e4065d5a335ac0842c254c63d523b778e2fb17
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2021-05-06 21:44:43 +0100

    update: design change

--- src/frontend/components/About/About.tsx
+++ src/frontend/components/About/About.tsx
@@ -5,6 +5,7 @@ import { Section } from '../UI/Section/Section';
 import { Container } from '../UI/Container/Container';
 
 import './About.scss';
+import './Code.scss';
 
 // type AboutProps = {};
 
@@ -12,8 +13,63 @@ export const About = (): JSX.Element => {
     return (
         <Section classes={['top', 'center-center']}>
             <Container classes={'about'}>
-                <article className='about__content'>
-                    <Heading importance={3} title={'About'} classes={['uppercase', 'spaced', 'underline']} />
+                <article className='code__container'>
+                    <p className='code__line'>
+                        <span className='code--keyword'>const</span> <span className='code--function'>About</span>{' '}
+                        <span className='code--symbol'>=</span> ( )<span className='code--symbol'>:</span>{' '}
+                        <span className='code--special'>string</span>[] <span className='code--keyword'>={'>'}</span>{' '}
+                        {'{'}
+                    </p>
+                    <p className='code__line'>
+                        <span className='code--keyword'>return</span> competenciesArr;
+                    </p>
+                    <p className='code__line'>{'}'}</p>
+                    {/* COMPETENCIES BLOCK */}
+                    <p className='code__line'>
+                        <span className='code--keyword'>const</span>{' '}
+                        <span className='code--function'>competencies</span> <span className='code--symbol'>=</span> [
+                    </p>
+                    <p className='code__line'>
+                        <span className='code--string'>&apos;HTML/CSS/JS&apos;</span>,{' '}
+                        <span className='code--string'>&apos;SASS&apos;</span>,{' '}
+                        <span className='code--string'>&apos;React&apos;</span>,{' '}
+                        <span className='code--string'>&apos;Redux&apos;</span>,{' '}
+                        <span className='code--string'>&apos;Jest/Enzyme&apos;</span>,{' '}
+                        <span className='code--string'>&apos;MySQL/NoSQL&apos;</span>,{' '}
+                        <span className='code--string'>&apos;Webpack&apos;</span>,{' '}
+                        <span className='code--string'>&apos;Node.js&apos;</span>,{' '}
+                        <span className='code--string'>&apos;npm&apos;</span>,{' '}
+                        <span className='code--string'>&apos;Express.js&apos;</span>,{' '}
+                        <span className='code--string'>&apos;Websockets/Socket.io&apos;</span>,{' '}
+                        <span className='code--string'>&apos;PWA&apos;</span>,{' '}
+                        <span className='code--string'>&apos;SPA&apos;</span>,{' '}
+                        <span className='code--string'>&apos;AWS/GCP&apos;</span>
+                    </p>
+                    <p className='code__line'>];</p>
+                    {/* INTERESTS BLOCK */}
+                    <p className='code__line'>
+                        <span className='code--keyword'>const</span> <span className='code--function'>interests</span>{' '}
+                        <span className='code--symbol'>=</span> [
+                    </p>
+                    <p className='code__line'>
+                        <span className='code--string'>&apos;Web Design&apos;</span>,{' '}
+                        <span className='code--string'>&apos;UI&apos;</span>,{' '}
+                        <span className='code--string'>&apos;UX&apos;</span>,{' '}
+                        <span className='code--string'>&apos;Frontend Development&apos;</span>,{' '}
+                        <span className='code--string'>&apos;Backend Development&apos;</span>,{' '}
+                        <span className='code--string'>&apos;Fullstack Development&apos;</span>,{' '}
+                        <span className='code--string'>&apos;Cloud Computing&apos;</span>,{' '}
+                        <span className='code--string'>&apos;Drones&apos;</span>,{' '}
+                        <span className='code--string'>&apos;Photography&apos;</span>,{' '}
+                        <span className='code--string'>&apos;Fitness&apos;</span>,{' '}
+                        <span className='code--string'>&apos;Nutrition&apos;</span>,{' '}
+                        <span className='code--string'>&apos;NFL&apos;</span>,{' '}
+                        <span className='code--string'>&apos;Chess&apos;</span>,{' '}
+                        <span className='code--string'>&apos;Music&apos;</span>,{' '}
+                        <span className='code--string'>&apos;Halo&apos;</span>,{' '}
+                    </p>
+                    <p className='code__line'>];</p>
+
                     <p>
                         Hi there! I&apos;m Sam, a 22-year-old Web Developer and Software Engineering graduate. I&apos;m
                         an enthusiastic guy with a passion for the web and its multitude of technologies.
@@ -31,35 +87,6 @@ export const About = (): JSX.Element => {
                         find me either exercising, playing with my dog, listening to music or being below average at
                         chess.
                     </p>
-                    <div className='about__content__lists-container'>
-                        <div className='about__content__lists-container__list'>
-                            <Heading importance={4} title={'Competencies:'} classes={['spaced']} />
-                            <ul className='wrap'>
-                                <li>HTML</li>
-                                <li>CSS & SASS</li>
-                                <li>JavaScript</li>
-                                <li>TypeScript</li>
-                                <li>React</li>
-                                <li>Redux</li>
-                                <li>Jest</li>
-                                <li>MySQL</li>
-                                <li>Webpack</li>
-                                <li>Express</li>
-                                <li>Socket.io</li>
-                            </ul>
-                        </div>
-                        <div className='about__content__lists-container__list'>
-                            <Heading importance={4} title={'Interests:'} classes={['spaced']} />
-                            <ul>
-                                <li>Web Design</li>
-                                <li>UI & UX</li>
-                                <li>Frontend Web Development</li>
-                                <li>Backend Development</li>
-                                <li>Fullstack Development</li>
-                                <li>Cloud Computing</li>
-                            </ul>
-                        </div>
-                    </div>
                 </article>
             </Container>
         </Section>
