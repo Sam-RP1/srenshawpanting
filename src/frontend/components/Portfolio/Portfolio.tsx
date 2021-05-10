4fa3efba50e4b4a1c34cb818d5e9bb8559f6d507
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2021-05-10 20:11:31 +0100

    update: design vision for portfolio page has changed and so has the html structure!

--- src/frontend/components/Portfolio/Portfolio.tsx
+++ src/frontend/components/Portfolio/Portfolio.tsx
@@ -1,21 +1,25 @@
 import React, { useEffect } from 'react';
-import { NavLink } from 'react-router-dom';
 import { gsap } from 'gsap';
 
-import { Heading } from '../UI/Typography/Heading/Heading';
+// Component Imports
 import { Section } from '../UI/Section/Section';
 import { Container } from '../UI/Container/Container';
+import { Heading } from '../UI/Typography/Heading/Heading';
 
 import { Icons } from '../../lib/interfaces';
 
+// Styles
 import './Portfolio.scss';
 // Could add a next and/or prev button to the timeline to jump between sections/segements using state and stuff when it is its own component
 
+// Types
 type PortfolioProps = {
     professionalItems: {
         id: string;
         title: string;
         created: string;
+        tag: string;
+        taster: string;
         description: string;
         techStack: string;
         buttons: Icons;
@@ -25,159 +29,62 @@ type PortfolioProps = {
         id: string;
         title: string;
         created: string;
-        description: string;
-        techStack?: string;
+        tag: string;
+        taster: string;
+        description?: string;
+        techStack: string;
         buttons: Icons;
     }[];
 };
 
+/**
+ * Exports Portfolio component
+ * @returns JSX.Element
+ */
 export const Portfolio = ({ professionalItems, academicItems }: PortfolioProps): JSX.Element => {
-    const professional = professionalItems.map((item, i) => {
-        const img = item.img;
-        return (
-            <div key={item.id} className='portfolio__panel'>
-                <div onClick={() => console.log('OPEN PROJECT')} className='portfolio__panel__container'>
-                    <div className='portfolio__panel__image' style={{ backgroundImage: 'url(' + img + ')' }}></div>
-                    <div className='portfolio__panel__glare'>
-                        <div className='portfolio__panel__glare__inner'></div>
-                    </div>
+    const proAspectRatios = ['ar-2', 'ar-1', 'ar-4', 'ar-1'];
+    const acaAspectRatios = ['ar-2', 'ar-4', 'ar-4', 'ar-1'];
 
-                    <div className='portfolio__panel__content'>
-                        <div className='portfolio__panel__info'>
-                            <div className='portfolio__panel__title'>
-                                <h5>{item.title}</h5>
-                            </div>
-                            <div className='portfolio__panel__detail'>
-                                <p className='portfolio__panel__int'>[{'0' + i}]</p>
-                                <p className='portfolio__panel__context'>{item.description}</p>
-                            </div>
-                        </div>
-                        {item.buttons.map((btn) => {
-                            return (
-                                <div key={btn.id} className='icon-wrapper'>
-                                    {btn.icon}
-                                </div>
-                            );
-                        })}
-                    </div>
+    const professional = professionalItems.map((item, i) => {
+        return (
+            <div key={item.id} className={'panel ' + 'panel--' + proAspectRatios[i]}>
+                <div className='panel__image' style={{ backgroundImage: 'url(' + item.img + ')' }}></div>
+                <div className='panel__content'>
+                    <h2 className='panel__content__title'>{item.title}</h2>
+                    <p className='panel__content__tag'>{item.tag}</p>
+                    <p className='panel__content__taster'>{item.taster}</p>
+                    <div className='panel__button'>{item.buttons[0].icon}</div>
                 </div>
             </div>
         );
     });
 
     const academic = academicItems.map((item, i) => {
-        const img = item.img;
         return (
-            <div key={item.id} className='portfolio__panel'>
-                <div onClick={() => console.log('OPEN PROJECT')} className='portfolio__panel__container'>
-                    <div className='portfolio__panel__image' style={{ backgroundImage: 'url(' + img + ')' }}></div>
-                    <div className='portfolio__panel__glare'>
-                        <div className='portfolio__panel__glare__inner'></div>
-                    </div>
-
-                    <div className='portfolio__panel__content'>
-                        <div className='portfolio__panel__info'>
-                            <div className='portfolio__panel__title'>
-                                <h5>{item.title}</h5>
-                            </div>
-                            <div className='portfolio__panel__detail'>
-                                <p className='portfolio__panel__int'>[{'0' + i}]</p>
-                                <p className='portfolio__panel__context'>{item.description}</p>
-                            </div>
-                        </div>
-                        {item.buttons.map((btn) => {
-                            return (
-                                <div key={btn.id} className='icon-wrapper'>
-                                    {btn.icon}
-                                </div>
-                            );
-                        })}
-                    </div>
+            <div key={item.id} className={'panel ' + 'panel--' + acaAspectRatios[i]}>
+                <div className='panel__image' style={{ backgroundImage: 'url(' + item.img + ')' }}></div>
+                <div className='panel__content'>
+                    <h2 className='panel__content__title'>{item.title}</h2>
+                    <p className='panel__content__tag'>{item.tag}</p>
+                    <p className='panel__content__taster'>{item.taster}</p>
+                    <div className='panel__button'>{item.buttons[0].icon}</div>
                 </div>
             </div>
         );
     });
 
-    useEffect(() => {
-        const ANGLE = 45;
-        // disable on small screen devices
-
-        const onMouseEnter = (elem) => {
-            elem.style.transition = `all 200ms cubic-bezier(0.3, 0.98, 0.52, 0.99)`;
-            setTimeout(() => {
-                elem.style.transition = 'initial';
-            }, 201);
-        };
-
-        const onMouseLeave = (elem) => {
-            elem.style.transition = `all 200ms cubic-bezier(0.3, 0.98, 0.52, 0.99)`;
-            elem.style.transform = `perspective(700px)
-                           rotateX(0deg)
-                           rotateY(0deg)
-                           rotateZ(0deg)`;
-            setTimeout(() => {
-                elem.style.transition = 'initial';
-            }, 201);
-        };
-
-        const onMouseMove = (e, parentElem, childElem) => {
-            const w = parentElem.clientWidth;
-            const h = parentElem.clientHeight;
-            const y = ((e.offsetX - w * 0.5) / w) * ANGLE;
-            const x = ((e.offsetY - h * 0.5) / h) * ANGLE;
-            gsap.to(childElem, {
-                duration: 0,
-                perspective: 700,
-                rotateX: x,
-                rotateY: y,
-                rotateZ: 0,
-                scale: 1.05,
-            });
-            // scale??
-        };
-
-        // make into non-anonymous functions
-        document.querySelectorAll('.portfolio__panel').forEach((panel) => {
-            const panelContainer = panel.querySelector('.portfolio__panel__container');
-            panel.addEventListener('mouseenter', () => onMouseEnter(panelContainer));
-
-            panel.addEventListener('mouseout', () => onMouseLeave(panelContainer));
-
-            panel.addEventListener('mousemove', (e) => onMouseMove(e, panel, panelContainer));
-        });
-
-        return () => {
-            document.querySelectorAll('.portfolio__panel').forEach((elem) => {
-                // elem.removeEventListener();
-            });
-        };
-    }, []);
-
     return (
-        <Section classes={['top', 'center-center']}>
+        <Section classes={['top', 'null-center']}>
             <Container classes='portfolio'>
                 <>
-                    <div className='portfolio__intro'>
-                        <div className='timeline'></div>
-                        <div className='portfolio__content-wrapper'>
-                            <div className='timeline__title-container'>
-                                <h3 className='timeline__title'>Portfolio{'/>'}</h3>
-                            </div>
-                            {/* <p>Select projects spanning both my professional and educational careers.</p> */}
-                            {professional}
-                        </div>
+                    <div className='portfolio__title'>
+                        <Heading importance={3} title='Portfolio' classes={['uppercase', 'spaced']} />
                     </div>
-
-                    <div className='portfolio__item'>
-                        <div className='timeline'></div>
-                        <div className='portfolio__content-wrapper'>
-                            <div className='timeline__title-container'>
-                                <h3 className='timeline__title'>Portfolio</h3>
-                            </div>
-
-                            {academic}
-                        </div>
+                    <div className='portfolio__mega-title'>
+                        <h1>Portfolio</h1>
                     </div>
+                    <div className='portfolio__container'>{professional}</div>
+                    <div className='portfolio__container'>{academic}</div>
                 </>
             </Container>
         </Section>
