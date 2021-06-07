9e364ca4f1a90b9830c942cdc1b6fbc62edb885f
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2021-06-07 23:13:22 +0100

    update: changed icons import to SocialIcons

--- src/frontend/containers/Connect/Connect.tsx
+++ src/frontend/containers/Connect/Connect.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 import { Connect as ConnectCmpnt } from '../../components/Connect/Connect';
 
-import { icons } from '../../lib/icons';
+import { socialIcons } from '../../lib/icons';
 
 // type ConnectProps = {};
 
 export const Connect = (): JSX.Element => {
-    return <ConnectCmpnt icons={icons} />;
+    return <ConnectCmpnt icons={socialIcons} />;
 };
