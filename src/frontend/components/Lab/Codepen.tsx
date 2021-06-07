fd6b9581e3c876cf6f52801119db29b1461be8c9
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2021-06-07 23:16:01 +0100

    created Codepen.tsx

---
+++ src/frontend/components/Lab/Codepen.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Codepen = (): JSX.Element => {
+    return (
+        <div className='lab__codepen'>
+            <h4 id='lab__codpen__title'>Codepen</h4>
+        </div>
+    );
+};
