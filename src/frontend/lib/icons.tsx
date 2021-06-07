1267176d607de6280e4fdf897b1d9710147cc8b9
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2021-06-07 23:11:22 +0100

    update: each icon is now exported individually as an Icon as well as a part of specific Icons arrays

--- src/frontend/lib/icons.tsx
+++ src/frontend/lib/icons.tsx
@@ -1,46 +1,94 @@
 import React from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChessKing, faMugHot } from '@fortawesome/free-solid-svg-icons';
-import { faLinkedinIn, faInstagram, faGithub, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faChessKing, faHandPointer } from '@fortawesome/free-solid-svg-icons';
+import {
+    faLinkedinIn,
+    faInstagram,
+    faGithub,
+    faYoutube,
+    faCodepen,
+    faFlickr,
+    faReact,
+} from '@fortawesome/free-brands-svg-icons';
 
-import { Icons } from './interfaces';
+import { Icon, Icons } from './interfaces';
 
-export const icons: Icons = [
-    {
-        id: 'github',
-        url: 'https://github.com/Sam-RP1',
-        icon: <FontAwesomeIcon icon={faGithub} />,
-        tooltip: 'GitHub',
-    },
-    {
-        id: 'linkedin',
-        url: 'https://www.linkedin.com/in/samuel-renshaw-panting/',
-        icon: <FontAwesomeIcon icon={faLinkedinIn} />,
-        tooltip: 'LinkedIn',
-    },
-    {
-        id: 'instagram',
-        url: 'https://www.instagram.com/sxm_rp/',
-        icon: <FontAwesomeIcon icon={faInstagram} />,
-        tooltip: 'Instagram',
-    },
-    {
-        id: 'youtube',
-        url: 'https://www.youtube.com/user/TheWarpCentral',
-        icon: <FontAwesomeIcon icon={faYoutube} />,
-        tooltip: 'YouTube',
-    },
-    {
-        id: 'chess',
-        url: 'https://www.chess.com/member/samuelr-p',
-        icon: <FontAwesomeIcon icon={faChessKing} />,
-        tooltip: 'Chess.com',
-    },
-    {
-        id: 'buymeacoffee',
-        url: 'https://www.buymeacoffee.com/samrp',
-        icon: <FontAwesomeIcon icon={faMugHot} />,
-        tooltip: 'BuyMeACoffee',
-    },
+export const githubIcon: Icon = {
+    id: 'github',
+    url: 'https://github.com/Sam-RP1',
+    icon: <FontAwesomeIcon icon={faGithub} />,
+    tooltip: 'GitHub',
+    extension: '/Sam-RP1',
+};
+
+export const codepenIcon: Icon = {
+    id: 'codepen',
+    url: 'https://codepen.io/sam-rp',
+    icon: <FontAwesomeIcon icon={faCodepen} />,
+    tooltip: 'Codepen',
+    extension: '/sam-rp',
+};
+
+export const linkedinIcon: Icon = {
+    id: 'linkedin',
+    url: 'https://www.linkedin.com/in/sam-rp/',
+    icon: <FontAwesomeIcon icon={faLinkedinIn} />,
+    tooltip: 'LinkedIn',
+    extension: '/in/sam-rp',
+};
+
+export const instagramIcon: Icon = {
+    id: 'instagram',
+    url: 'https://www.instagram.com/sxm_rp/',
+    icon: <FontAwesomeIcon icon={faInstagram} />,
+    tooltip: 'Instagram',
+    extension: '/sxm_rp',
+};
+
+export const youtubeIcon: Icon = {
+    id: 'youtube',
+    url: 'https://www.youtube.com/c/warpSRP',
+    icon: <FontAwesomeIcon icon={faYoutube} />,
+    tooltip: 'YouTube',
+    extension: '/c/warpSRP',
+};
+
+export const flickrIcon: Icon = {
+    id: 'flickr',
+    url: 'https://www.flickr.com/photos/sam-rp/',
+    icon: <FontAwesomeIcon icon={faFlickr} />,
+    tooltip: 'flickr',
+    extension: '/photos/sam-rp',
+};
+
+export const chessIcon: Icon = {
+    id: 'chess',
+    url: 'https://www.chess.com/member/sam-rp',
+    icon: <FontAwesomeIcon icon={faChessKing} />,
+    tooltip: 'Chess.com',
+    extension: '/member/sam-rp',
+};
+
+export const pointerIcon: Icon = {
+    id: 'pointer',
+    icon: <FontAwesomeIcon icon={faHandPointer} />,
+    tooltip: 'Press',
+};
+
+export const reactIcon: Icon = {
+    id: 'react-icon',
+    icon: <FontAwesomeIcon icon={faReact} />,
+};
+
+export const socialIcons: Icons = [
+    githubIcon,
+    codepenIcon,
+    linkedinIcon,
+    instagramIcon,
+    youtubeIcon,
+    flickrIcon,
+    chessIcon,
 ];
+
+export const footerIcons: Icons = [githubIcon, codepenIcon, linkedinIcon, instagramIcon, chessIcon];
