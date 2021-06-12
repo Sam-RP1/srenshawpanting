99bb5e35ee95042eb1bc83ce915f12f7a494813e
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2021-06-12 20:35:59 +0100

    update: removed unused content

--- src/frontend/components/About/About.tsx
+++ src/frontend/components/About/About.tsx
@@ -68,49 +68,6 @@ export const About = (): JSX.Element => {
                             })}
                         </div>
                     </div>
-
-                    <article className='code__container'>
-                        {/* DATA BLOCK */}
-                        <p className='code__line'>
-                            <span className='code--keyword'>const</span> <span className='code--function'>srpData</span>{' '}
-                            <span className='code--symbol'>=</span>
-                            {' {'}
-                        </p>
-                        <p className='code__line code__line--indent--4'>
-                            name: <span className='code--string'>&apos;Samuel Renshaw-Panting&apos;</span>,
-                        </p>
-                        <p className='code__line code__line--indent--4'>
-                            age: <span className='code--integer'>22</span>,
-                        </p>
-                        <p className='code__line code__line--indent--4'>
-                            gender: <span className='code--string'>&apos;Male&apos;</span>,
-                        </p>
-                        <p className='code__line code__line--indent--4'>
-                            dateOfBirth: <span className='code--integer'>906726600000</span>,
-                        </p>
-                        <p className='code__line code__line--indent--4'>
-                            email: <span className='code--string'>&apos;srenshawpanting@gmail.com&apos;</span>,
-                        </p>
-                        <p className='code__line'>{'}'};</p>
-                        <p className='code__line'>&nbsp;</p>
-
-                        {/* INTERESTS BLOCK */}
-                        <p className='code__line'>
-                            <span className='code--keyword'>const</span>{' '}
-                            <span className='code--function'>interests</span> <span className='code--symbol'>=</span> [
-                        </p>
-                        <p className='code__line code__string code__line--indent--4'>
-                            <span>&apos;Web Design&apos;</span>, <span>&apos;UI&apos;</span>,{' '}
-                            <span>&apos;UX&apos;</span>, <span>&apos;Frontend Development&apos;</span>,{' '}
-                            <span>&apos;Backend Development&apos;</span>, <span>&apos;Fullstack Development&apos;</span>
-                            , <span>&apos;Cloud Computing&apos;</span>, <span>&apos;Drones&apos;</span>,{' '}
-                            <span>&apos;Photography&apos;</span>, <span>&apos;Fitness&apos;</span>,{' '}
-                            <span>&apos;Nutrition&apos;</span>, <span>&apos;NFL&apos;</span>,{' '}
-                            <span>&apos;Chess&apos;</span>, <span>&apos;Music&apos;</span>,{' '}
-                            <span>&apos;Halo&apos;</span>
-                        </p>
-                        <p className='code__line'>];</p>
-                    </article>
                 </>
             </Container>
         </Section>
