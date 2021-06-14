d1a35b108461581d19725588fa57bace14f74488
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2021-06-14 18:12:10 +0100

    update: moved moon and moonlight into a container for easier positioning and resizing

--- src/frontend/components/Background/Background.tsx
+++ src/frontend/components/Background/Background.tsx
@@ -6,17 +6,19 @@ import moon from '../../assets/svgs/moontest.svg';
 export const Background = (): JSX.Element => {
     return (
         <div id='bg'>
-            <div className='moon'>
-                <div className='moon__container'>
-                    <div className='moon__layer'>
-                        <img className='moon__img' src={moon} />
+            <div className='moon-container'>
+                <div className='moon'>
+                    <div className='moon__container'>
+                        <div className='moon__layer'>
+                            <img className='moon__img' src={moon} />
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className='moonlight'>
-                <div className='moonlight__container'>
-                    <div className='moonlight__wrap'>
-                        <div className='moonlight__img'></div>
+                <div className='moonlight'>
+                    <div className='moonlight__container'>
+                        <div className='moonlight__wrap'>
+                            <div className='moonlight__img'></div>
+                        </div>
                     </div>
                 </div>
             </div>
