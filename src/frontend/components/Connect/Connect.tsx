da660a47b850d320c6b5f961f7c55d1f35869670
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2021-06-17 20:35:18 +0100

    update: implemented optional leaderLine props used currently for Connect.tsx to draw SVG lines between points

--- src/frontend/components/Connect/Connect.tsx
+++ src/frontend/components/Connect/Connect.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import LeaderLine from 'react-leader-line';
 
 import { Section } from '../UI/Section/Section';
 import { Container } from '../UI/Container/Container';
@@ -16,7 +17,7 @@ type ConnectProps = {
 export const Connect = ({ icons }: ConnectProps): JSX.Element => {
     const moonOrbits = ['orbit-1', 'orbit-2', 'orbit-3', 'orbit-4', 'orbit-5', 'orbit-6', 'orbit-7', 'orbit-8'];
 
-    const connectPlanets = icons.map(({ id, icon, url }) => {
+    const connectPlanets = icons.map(({ id, icon, url }, i) => {
         const index = Math.floor(Math.random() * moonOrbits.length);
         const orbit = moonOrbits.splice(index, 1);
 
@@ -34,8 +35,10 @@ export const Connect = ({ icons }: ConnectProps): JSX.Element => {
 
                 <div className='connect__planet-container'>
                     <div className='connect__planet'>
-                        <a href={url} target='__blank' className='connect__planet__body' id={'connect-planet-' + id}>
-                            <span className='connect__planet__body__icon'>{icon}</span>
+                        <a href={url} target='__blank' className={'connect__planet__body connect__planet__body--' + id}>
+                            <span id={'connect-planet-' + i} className='connect__planet__body__icon'>
+                                {icon}
+                            </span>
                         </a>
                     </div>
                 </div>
@@ -43,11 +46,72 @@ export const Connect = ({ icons }: ConnectProps): JSX.Element => {
         );
     });
 
+    useEffect(() => {
+        const defaultLineProperties = {
+            path: 'fluid',
+            color: '#0080b3',
+            size: 2,
+            startPlug: 'behind',
+            endPlug: 'behind',
+        };
+        const lineArr = [];
+
+        const createLines = () => {
+            const startLine = new LeaderLine(
+                document.getElementById('connect-leader-line-start'),
+                document.getElementById('connect-planet-0'),
+                defaultLineProperties
+            );
+            lineArr.push(startLine);
+
+            const endLine = new LeaderLine(
+                document.getElementById('connect-planet-' + (icons.length - 1)),
+                document.getElementById('connect-leader-line-end'),
+                defaultLineProperties
+            );
+            lineArr.push(endLine);
+
+            for (let i = 0; i < icons.length; i++) {
+                if (i + 1 !== icons.length) {
+                    const line = new LeaderLine(
+                        document.getElementById('connect-planet-' + i),
+                        document.getElementById('connect-planet-' + (i + 1)),
+                        defaultLineProperties
+                    );
+                    lineArr.push(line);
+                }
+            }
+        };
+
+        const repositionLines = () => {
+            for (const line in lineArr) {
+                lineArr[line].position();
+            }
+        };
+
+        const destroyLines = () => {
+            for (const line in lineArr) {
+                lineArr[line].remove();
+            }
+        };
+
+        createLines();
+        repositionLines();
+        window.addEventListener('resize', repositionLines, false);
+        window.addEventListener('scroll', repositionLines, false);
+
+        return () => {
+            window.removeEventListener('resize', repositionLines);
+            window.removeEventListener('scroll', repositionLines);
+            destroyLines();
+        };
+    }, []);
+
     return (
         <Section selector={'connect'} classes={[]}>
             <Container classes={'connect'}>
                 <>
-                    <Timeline title={'Connect'} megaTitle={true} />
+                    <Timeline title={'connect'} megaTitle={true} leaderLine={true} />
 
                     <div className='connect__planets-container'>{connectPlanets}</div>
                 </>
--- src/frontend/components/UI/Timeline/Timeline.tsx
+++ src/frontend/components/UI/Timeline/Timeline.tsx
@@ -5,14 +5,16 @@ import './Timeline.scss';
 type TimelineProps = {
     title: string;
     megaTitle?: boolean;
+    leaderLine?: boolean;
 };
 
-export const Timeline = ({ title, megaTitle = true }: TimelineProps): JSX.Element => {
+export const Timeline = ({ title, megaTitle = true, leaderLine = false }: TimelineProps): JSX.Element => {
     return (
         <>
-            <div className='timeline'></div>
+            <div className='timeline'>{leaderLine === true && <span id={title + '-leader-line-end'}></span>}</div>
             <div className='timeline__title'>
                 <h1>{title}</h1>
+                {leaderLine === true && <span id={title + '-leader-line-start'}></span>}
                 {megaTitle === true && (
                     <div className='mega-title'>
                         <h2>{title}</h2>
