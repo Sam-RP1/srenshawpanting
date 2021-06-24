13a324fda62ef3ea61db75c20282bab7aa0e69b4
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2021-06-24 18:27:54 +0100

    update: minor changes to Flickr glide carousel

--- src/frontend/components/Lab/Flickr.tsx
+++ src/frontend/components/Lab/Flickr.tsx
@@ -17,27 +17,32 @@ export const Flickr = ({ flickrContent }: FlickrProps): JSX.Element => {
 
     const slideContainer = useRef(null);
 
-    const slideElems = flickrContent.map((photo, i) => {
+    const slideElems = flickrContent.map(({ url }, i) => {
         return (
             <li key={'lab-flickr-slide-' + i} className='glide__slide'>
-                <img src={photo.url} />
+                <img src={url} />
             </li>
         );
     });
 
     useEffect(() => {
-        // console.log('FLICKR USE EFFECT');
         if (slideContainer.current.hasChildNodes()) {
             const carousel = new Glide('.glide', {
                 type: 'carousel',
                 focusAt: 'center',
-                startAt: 1,
-                perView: 2,
+                startAt: 0,
+                perView: 1.7,
                 breakpoints: {
-                    1024: {
-                        perView: 2,
+                    1475: {
+                        perView: 1.5,
                     },
-                    600: {
+                    912: {
+                        perView: 1.3,
+                    },
+                    767: {
+                        perView: 1.15,
+                    },
+                    575: {
                         perView: 1,
                     },
                 },
@@ -48,30 +53,37 @@ export const Flickr = ({ flickrContent }: FlickrProps): JSX.Element => {
 
     return (
         <div className='lab__flickr'>
-            <h4 id='lab__flickr__title'>Flickr</h4>
-
-            <div className='glide'>
-                <div className='glide__track' data-glide-el='track'>
-                    <ul ref={slideContainer} className='glide__slides'>
-                        {slideElems}
-                    </ul>
+            <div className='lab__title'>
+                <h1>Flickr</h1>
+                <span id='flickr-leader-line'></span>
+                <div className='mega-title'>
+                    <h2>Flickr</h2>
                 </div>
+            </div>
 
-                <div className='glide__arrows' data-glide-el='controls'>
-                    <button className='glide__arrow glide__arrow--left' data-glide-dir='<'>
-                        prev
-                    </button>
-                    <button className='glide__arrow glide__arrow--right' data-glide-dir='>'>
-                        next
-                    </button>
+            <div className='glide-wrapper'>
+                <div className='glide'>
+                    <div className='glide__track' data-glide-el='track'>
+                        <ul ref={slideContainer} className='glide__slides'>
+                            {/* {slideElems} */}
+                            {/* Lazy loading spinners inside each slide then when loaded replace the spinners */}
+                            <li className='glide__slide'></li>
+                            <li className='glide__slide'></li>
+                            <li className='glide__slide'></li>
+                            <li className='glide__slide'></li>
+                            <li className='glide__slide glide__slide--more'></li>
+                        </ul>
+                    </div>
+
+                    <div className='glide__bullets' data-glide-el='controls[nav]'>
+                        <button className='glide__bullet' data-glide-dir='=0'></button>
+                        <button className='glide__bullet' data-glide-dir='=1'></button>
+                        <button className='glide__bullet' data-glide-dir='=2'></button>
+                        <button className='glide__bullet' data-glide-dir='=3'></button>
+                        <button className='glide__bullet' data-glide-dir='=4'></button>
+                    </div>
                 </div>
-                {/* 
-                <div className='glide__bullets' data-glide-el='controls[nav]'>
-                    <button className='glide__bullet' data-glide-dir='=0'></button>
-                    <button className='glide__bullet' data-glide-dir='=1'></button>
-                    <button className='glide__bullet' data-glide-dir='=2'></button>
-                    <button className='glide__bullet' data-glide-dir='=3'></button>
-                </div> */}
+                <span id='flickr-leader-line-bottom'></span>
             </div>
         </div>
     );
