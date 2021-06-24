89ccfc849ea333bec208fa44c42e2303b372e1fc
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2021-06-24 18:28:32 +0100

    update: implemented leader line

--- src/frontend/components/Lab/Github.tsx
+++ src/frontend/components/Lab/Github.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { githubIcon, externalLinkIcon } from '../../lib/icons';
 
 type GithubProps = {
     featured: {
@@ -22,51 +24,79 @@ type GithubProps = {
 export const Github = ({ featured, recent }: GithubProps): JSX.Element => {
     console.log('render GITHUB component');
 
+    const [openTab, setOpenTab] = useState('featured');
+
+    const featuredRepos = featured.map(({ id, title, repoURL, webURL }) => (
+        <div key={'lab-featured-repo-' + id} className='lab__github__panel__repo'>
+            <p>{title}</p>
+            <div className='lab__github__panel__repo__icons'>
+                {webURL !== '' && webURL !== null && (
+                    <a href={webURL} target='__blank'>
+                        {externalLinkIcon.icon}
+                    </a>
+                )}
+                <a href={repoURL} target='__blank'>
+                    {githubIcon.icon}
+                </a>
+            </div>
+        </div>
+    ));
+
+    const recentRepos = recent.map(({ id, title, repoURL, webURL }) => (
+        <div key={'lab-recent-repo-' + id} className='lab__github__panel__repo'>
+            <p>{title}</p>
+            <div className='lab__github__panel__repo__icons'>
+                {webURL !== '' && webURL !== null && (
+                    <a href={webURL} target='__blank'>
+                        {externalLinkIcon.icon}
+                    </a>
+                )}
+                <a href={repoURL} target='__blank'>
+                    {githubIcon.icon}
+                </a>
+            </div>
+        </div>
+    ));
+
+    const tabs = [
+        { id: 'featured', content: featuredRepos },
+        { id: 'recent', content: recentRepos },
+    ];
+
     // put the new tool tips on the icons
     // when doing icons check for "" and null for the website link not the repo link
 
-    const featuredRepos = featured.map((repo) => {
-        return (
-            <div key={'lab-featured-repo-' + repo.id} className='lab__github__panel__repo'>
-                <p>{repo.title}</p>
-                <div className='lab__github__Panel__repo__icons'></div>
-            </div>
-        );
-    });
-
-    const recentRepos = recent.map((repo) => {
-        return (
-            <div key={'lab-featured-repo-' + repo.id} className='lab__github__panel__repo'>
-                <p>{repo.title}</p>
-                <div className='lab__github__Panel__repo__icons'></div>
-            </div>
-        );
-    });
-
     return (
         <div className='lab__github'>
-            <h4 id='lab__title'>Github</h4>
+            <div className='lab__title'>
+                <h1>Github</h1>
+                <span id='github-leader-line'></span>
+                <div className='mega-title'>
+                    <h2>Github</h2>
+                </div>
+            </div>
 
             <div className='lab__github__panel'>
                 <section className='lab__github__panel__tabs'>
-                    <div className='lab__github__panel__tab'>Featured</div>
-                    <div className='lab__github__panel__tab'>Recent</div>
+                    {tabs.map(({ id }) => {
+                        const isActive = id === openTab ? ' active' : '';
+                        return (
+                            <div
+                                key={id}
+                                onClick={() => setOpenTab(id)}
+                                className={'lab__github__panel__tab' + isActive}
+                            >
+                                <p>{id}</p>
+                            </div>
+                        );
+                    })}
                 </section>
 
                 <section className='lab__github__panel__content'>
-                    <div className='lab__github__panel__featured'>
-                        <div className='lab__github__panel__repos'>{featuredRepos}</div>
-                        <div className='lab__github__panel__title'>
-                            <h4>Featured</h4>
-                        </div>
-                    </div>
-
-                    <div className='lab__github__panel__recent'>
-                        <div className='lab__github__panel__repos'>{recentRepos}</div>
-                        <div className='lab__github__panel__title'>
-                            <h4>Recent</h4>
-                        </div>
-                    </div>
+                    {tabs.map(({ id, content }) => {
+                        const repos = id === openTab ? content : '';
+                        return repos;
+                    })}
                 </section>
             </div>
         </div>
--- src/frontend/components/Lab/Lab.tsx
+++ src/frontend/components/Lab/Lab.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-// import LeaderLine from 'react-leader-line';
+import LeaderLine from 'react-leader-line';
 
 import { Timeline } from '../UI/Timeline/Timeline';
 
@@ -8,20 +8,68 @@ import './Lab.scss';
 export const Lab = (): JSX.Element => {
     console.log('render LAB component');
     useEffect(() => {
-        // const test = document.querySelector('#line-start');
-        // const githubAnchor = document.querySelector('.lab__github');
-        // const codepenAnchor = document.querySelector('.lab__codepen');
-        // const flickrAnchor = document.querySelector('.lab__flickr');
-        // new LeaderLine(test, codepenAnchor, {
-        //     color: '#0080b3',
-        //     size: 2,
-        //     startPlug: 'behind',
-        //     endPlug: 'behind',
-        //     startSocket: 'left',
-        //     endSocket: 'left',
-        //     startSocketGravity: '500',
-        // });
+        const defaultLineProperties = {
+            path: 'fluid',
+            color: '#0080b3',
+            size: 2,
+            startPlug: 'behind',
+            endPlug: 'behind',
+        };
+        const lineArr = [];
+
+        const createLines = () => {
+            const startLine = new LeaderLine(
+                document.getElementById('lab-leader-line-start'),
+                document.getElementById('github-leader-line'),
+                defaultLineProperties
+            );
+            lineArr.push(startLine);
+
+            const joiningLine = new LeaderLine(
+                document.getElementById('github-leader-line'),
+                document.getElementById('flickr-leader-line'),
+                defaultLineProperties
+            );
+            lineArr.push(joiningLine);
+
+            const joiningLine2 = new LeaderLine(
+                document.getElementById('flickr-leader-line'),
+                document.getElementById('flickr-leader-line-bottom'),
+                defaultLineProperties
+            );
+            lineArr.push(joiningLine2);
+
+            const endLine = new LeaderLine(
+                document.getElementById('flickr-leader-line-bottom'),
+                document.getElementById('lab-leader-line-end'),
+                defaultLineProperties
+            );
+            lineArr.push(endLine);
+        };
+
+        const repositionLines = () => {
+            for (const line in lineArr) {
+                lineArr[line].position();
+            }
+        };
+
+        const destroyLines = () => {
+            for (const line in lineArr) {
+                lineArr[line].remove();
+            }
+        };
+
+        createLines();
+        repositionLines();
+        window.addEventListener('resize', repositionLines, false);
+        window.addEventListener('scroll', repositionLines, false);
+
+        return () => {
+            window.removeEventListener('resize', repositionLines);
+            window.removeEventListener('scroll', repositionLines);
+            destroyLines();
+        };
     }, []);
 
-    return <Timeline title={'Lab'} megaTitle={true} />;
+    return <Timeline title={'lab'} megaTitle={true} leaderLine={true} />;
 };
