865fb9d5be3bae018ea393d400fcc45e8e903feb
Sam R-P <44971801+Sam-RP1@users.noreply.github.com>
2021-09-28 20:22:37 +0100

    update: changing architecture to use browser router for multiple pages, tbd if this will be permanent approach

--- src/frontend/app.tsx
+++ src/frontend/app.tsx
@@ -1,27 +1,34 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
+// Pages
+import { Home } from './pages/Home/Home';
+import { Showcase } from './pages/Showcase/Showcase';
+
+// Page Components
 import { Cursor } from './components/Cursor/Cursor';
-import { Header } from './containers/Header/Header';
-import { Hero } from './containers/Hero/Hero';
-import { About } from './containers/About/About';
-import { Portfolio } from './containers/Portfolio/Portfolio';
-import { Lab } from './containers/Lab/Lab';
-import { Connect } from './containers/Connect/Connect';
 import { Footer } from './containers/Footer/Footer';
 import { Background } from './components/Background/Background';
 
+// Showcase data
+import { deductionData } from './assets/projects/deduction/deduction';
+
+/**
+ * Exports App
+ * @returns JSX.Element
+ */
 export const App = (): JSX.Element => {
     return (
         <section data-test='component-app'>
             <Background />
-            <Header />
-            <Hero />
-            <About />
-            <Portfolio />
-            <Lab />
-            <Connect />
+            <BrowserRouter>
+                <Route path='/thedeductiongame' exact render={() => <Showcase data={deductionData} />} />
+                <Route path='/' exact component={Home} />
+            </BrowserRouter>
             <Footer />
             <Cursor />
         </section>
     );
 };
+
+// Need an error route
