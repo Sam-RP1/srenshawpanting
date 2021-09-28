58122b8d0c01b781402919cc918919f080647572
Sam R-P <44971801+Sam-RP1@users.noreply.github.com>
2021-09-28 20:20:44 +0100

    update: type now imported, changed destructuring for portfolio content

--- src/frontend/components/Portfolio/Portfolio.tsx
+++ src/frontend/components/Portfolio/Portfolio.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 // Component Imports
 import { Section } from '../UI/Section/Section';
@@ -6,92 +6,57 @@ import { Container } from '../UI/Container/Container';
 import { Timeline } from '../UI/Timeline/Timeline';
 import { Spinner } from '../UI/Spinner/Spinner';
 
-import { Icon } from '../../lib/interfaces';
-
 // Styles
 import './Portfolio.scss';
 
 // Types
-type PortfolioProps = {
-    portfolioContent: {
-        id: string;
-        title: string;
-        created: string;
-        taster: string;
-        description: string;
-        tags: string[];
-        buttons: {
-            id: string;
-            url: string;
-            icon: Icon;
-            label: string;
-        }[];
-        img: string;
-    }[];
-};
+import { ProjectsType } from '../../lib/types';
 
 /**
  * Exports Portfolio component
  * @returns JSX.Element
  */
-export const Portfolio = ({ portfolioContent }: PortfolioProps): JSX.Element => {
+export const Portfolio = ({ portfolioContent }: ProjectsType): JSX.Element => {
     const [isLoading, setIsLoading] = useState(false);
     const [images, setImages] = useState(undefined);
-    const aspectRatios = ['ar-4', 'ar-1', 'ar-3', 'ar-1', 'ar-4', 'ar-3', 'ar-4', 'ar-2'];
+    const aspectRatios = ['ar-4', 'ar-1', 'ar-3', 'ar-1', 'ar-4', 'ar-3', 'ar-1', 'ar-4', 'ar-2'];
 
     console.log(portfolioContent[0]);
-    // https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement/complete
-    // https://stackoverflow.com/questions/43115246/how-to-detect-when-a-image-is-loaded-that-is-provided-via-props-and-change-sta
-    // useEffect(() => {
-    //     if (portfolioContent.img.length !== 0) {
-    //         setIsLoading(false);
-    //         setImages(flickrContent);
-    //     } else {
-    //         setIsLoading(true);
-    //     }
 
-    //     const splide = new Splide('.splide', {
-    //         type: 'slide',
-    //         rewind: 'true', // rewind from 1 to last or last to 1
-    //         height: 295,
-    //         autoHeight: false,
-    //         width: 500,
-    //         autoWidth: false,
-    //         gap: 10,
-    //         arrows: false,
-    //         focus: 'center',
-    //     }).mount();
+    const content = portfolioContent.map(
+        ({ id, title, portfolio: { thumbnail }, definition, tags, portfolio: { buttons } }, i) => (
+            <div key={id} id={id} className={'panel ' + 'panel--' + aspectRatios[i]}>
+                <div className='panel__image'>{isLoading ? <Spinner /> : <img src={thumbnail} />}</div>
+                <div className='panel__content'>
+                    <h2 className='panel__content__title'>{title}</h2>
 
-    //     return () => {
-    //         splide.destroy();
-    //     };
-    // }, [portfolioContent]);
+                    <p className='panel__content__taster'>{definition}</p>
 
-    const content = portfolioContent.map(({ id, title, img, taster, tags, buttons }, i) => (
-        <div key={id} id={id} className={'panel ' + 'panel--' + aspectRatios[i]}>
-            <div className='panel__image'>{isLoading ? <Spinner /> : <img src={img} />}</div>
-            <div className='panel__content'>
-                <h2 className='panel__content__title'>{title}</h2>
+                    <div className='panel__content__tags'>
+                        {tags.map((tag) => (
+                            <p key={id + '-tag-' + tag}>{tag}</p>
+                        ))}
+                    </div>
 
-                <p className='panel__content__taster'>{taster}</p>
-
-                <div className='panel__content__tags'>
-                    {tags.map((tag) => (
-                        <p key={id + '-tag-' + tag}>{tag}</p>
-                    ))}
-                </div>
-
-                <div className='panel__content__buttons'>
-                    {buttons.map(({ id, url, icon, label }) => (
-                        <a key={id} href={url} target='__blank'>
-                            <span>{label}</span>
-                            {icon.icon}
-                        </a>
-                    ))}
+                    <div className='panel__content__buttons'>
+                        {buttons.map(({ id, url, icon, label, newTab }) =>
+                            newTab === true ? (
+                                <a key={id} href={url} target='__blank'>
+                                    <span>{label}</span>
+                                    {icon.icon}
+                                </a>
+                            ) : (
+                                <a key={id} href={url}>
+                                    <span>{label}</span>
+                                    {icon.icon}
+                                </a>
+                            )
+                        )}
+                    </div>
                 </div>
             </div>
-        </div>
-    ));
+        )
+    );
 
     // Add in mega-titles that are absolutely positioned but are translated to their require position (eg 50% of the way down)
     // Keyboard compatability
