bf1644e0ce8b9fae5be69d9d5c63b2051084b765
Sam R-P <44971801+Sam-RP1@users.noreply.github.com>
2021-10-09 22:27:18 +0100

    update: added modifiers to Timeline, specifically "inverse" for titles on light coloured backgrounds

--- src/frontend/components/UI/Timeline/Timeline.tsx
+++ src/frontend/components/UI/Timeline/Timeline.tsx
@@ -6,13 +6,23 @@ type TimelineProps = {
     title: string;
     megaTitle?: boolean;
     leaderLine?: boolean;
+    modifiers?: string[];
 };
 
-export const Timeline = ({ title, megaTitle = true, leaderLine = false }: TimelineProps): JSX.Element => {
+export const Timeline = ({
+    title,
+    megaTitle = true,
+    leaderLine = false,
+    modifiers = [],
+}: TimelineProps): JSX.Element => {
+    let classes = '';
+    for (const modifier in modifiers) {
+        classes += ' timeline__title--' + modifiers[modifier];
+    }
     return (
         <>
             <div className='timeline'>{leaderLine === true && <span id={title + '-leader-line-end'}></span>}</div>
-            <div className='timeline__title'>
+            <div className={'timeline__title' + classes}>
                 <h1>{title}</h1>
                 {leaderLine === true && <span id={title + '-leader-line-start'}></span>}
                 {megaTitle === true && (
