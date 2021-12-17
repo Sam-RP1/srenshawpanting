f7bd7131f41f21fb03de5c6b35f69861f2be892a
Sam R-P <44971801+Sam-RP1@users.noreply.github.com>
2021-12-17 14:21:54 +0000

    update: updated import

--- src/frontend/containers/Portfolio/Portfolio.tsx
+++ src/frontend/containers/Portfolio/Portfolio.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { Portfolio as PortfolioCmpnt } from '../../components/Portfolio/Portfolio';
 
-import { projectsData } from '../../assets/projects/projects';
+import { projectsData } from '../../assets/projects/index';
 
 export const Portfolio = (): JSX.Element => {
     return <PortfolioCmpnt portfolioContent={projectsData} />;
