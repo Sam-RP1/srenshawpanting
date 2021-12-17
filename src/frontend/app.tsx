f2bbe04e142b1ff217716420efcb3bd6b198afba
Sam R-P <44971801+Sam-RP1@users.noreply.github.com>
2021-12-17 14:21:00 +0000

    update: removed pages in this version

--- src/frontend/app.tsx
+++ src/frontend/app.tsx
@@ -1,34 +1,41 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-
-// Pages
-import { Home } from './pages/Home/Home';
-import { Showcase } from './pages/Showcase/Showcase';
 
 // Page Components
+import { Header } from './containers/Header/Header';
+import { Hero } from './containers/Hero/Hero';
+import { About } from './containers/About/About';
+import { Portfolio } from './containers/Portfolio/Portfolio';
+import { Lab } from './containers/Lab/Lab';
+import { Connect } from './containers/Connect/Connect';
 import { Cursor } from './components/Cursor/Cursor';
 import { Footer } from './containers/Footer/Footer';
 import { Background } from './components/Background/Background';
-
-// Showcase data
-import { deductionData } from './assets/projects/deduction/deduction';
 
 /**
  * Exports App
  * @returns JSX.Element
  */
 export const App = (): JSX.Element => {
+    const headerBtns = ['about', 'portfolio', 'lab', 'connect'];
+    const heroTitle = 'Welcome';
+    const heroContent = (
+        <div className='hero__content__text'>
+            <p>Hi, I&apos;m Sam, a Full Stack Web Developer & Software Engineer</p>
+        </div>
+    );
+    const heroNextItemLabel = "Let's get going...";
+
     return (
         <section data-test='component-app'>
             <Background />
-            <BrowserRouter>
-                <Route path='/thedeductiongame' exact render={() => <Showcase data={deductionData} />} />
-                <Route path='/' exact component={Home} />
-            </BrowserRouter>
+            <Header btns={headerBtns} />
+            <Hero title={heroTitle} content={heroContent} nextItemLabel={heroNextItemLabel} />
+            <About />
+            <Portfolio />
+            <Lab />
+            <Connect />
             <Footer />
             <Cursor />
         </section>
     );
 };
-
-// Need an error route
