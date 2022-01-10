e62e151312467add526696085c26f88369f9a9ea
Sam R-P <44971801+Sam-RP1@users.noreply.github.com>
2022-01-10 15:24:04 +0000

    update: removed taster property

--- src/frontend/components/Portfolio/Portfolio.tsx
+++ src/frontend/components/Portfolio/Portfolio.tsx
@@ -22,13 +22,14 @@ export const Portfolio = ({ portfolioContent }: ProjectsType): JSX.Element => {
     const aspectRatios = ['ar-16x9', 'ar-4x5', 'ar-4x3', 'ar-16x9', 'ar-16x9', 'ar-4x5', 'ar-4x3', 'ar-16x9', 'ar-4x5'];
 
     const content = portfolioContent.map(
-        ({ id, title, portfolio: { thumbnail }, definition, tags, portfolio: { buttons } }, i) => (
+        ({ id, title, portfolio: { thumbnail }, definition, taster, tags, portfolio: { buttons } }, i) => (
             <div key={id} id={id} className={'panel ' + 'panel--' + aspectRatios[i]}>
                 <div className='panel__image'>{isLoading ? <Spinner /> : <img src={thumbnail} />}</div>
                 <div className='panel__content'>
                     <h2 className='panel__content__title'>{title}</h2>
 
-                    <p className='panel__content__taster'>{definition}</p>
+                    <p className='panel__content__definition'>{definition}</p>
+                    <p className='panel__content__taster'>{taster}</p>
 
                     <div className='panel__content__tags'>
                         {tags.map((tag) => (
@@ -37,15 +38,13 @@ export const Portfolio = ({ portfolioContent }: ProjectsType): JSX.Element => {
                     </div>
 
                     <div className='panel__content__buttons'>
-                        {buttons.map(({ id, url, icon, label, newTab }) =>
+                        {buttons.map(({ id, url, icon, newTab }) =>
                             newTab === true ? (
                                 <a key={id} href={url} target='__blank'>
-                                    <span>{label}</span>
                                     {icon.icon}
                                 </a>
                             ) : (
                                 <a key={id} href={url}>
-                                    <span>{label}</span>
                                     {icon.icon}
                                 </a>
                             )
